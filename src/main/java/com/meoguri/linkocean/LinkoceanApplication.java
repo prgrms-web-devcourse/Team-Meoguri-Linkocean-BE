cd57ffa3612058114cd49d9f45044aa7430c61f8
Juhan <juhan211@naver.com>
2022-07-21 17:15:07 +0900

    init: 초기 세팅

---
+++ src/main/java/com/meoguri/linkocean/LinkoceanApplication.java
@@ -0,0 +1,13 @@
+package com.meoguri.linkocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkoceanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkoceanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meoguri/linkocean/LinkoceanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meoguri.linkocean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkoceanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
