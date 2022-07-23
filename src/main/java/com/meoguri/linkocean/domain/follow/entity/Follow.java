663c4c5740f40b64bc514f41eb00433998b77e75
ndy2 <emrdbs12@gmail.com>
2022-07-23 21:19:00 +0900

    feat: add unique constraint

--- src/main/java/com/meoguri/linkocean/domain/follow/entity/Follow.java
+++ src/main/java/com/meoguri/linkocean/domain/follow/entity/Follow.java
@@ -5,6 +5,8 @@ import static lombok.AccessLevel.*;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.meoguri.linkocean.domain.common.BaseIdEntity;
 import com.meoguri.linkocean.domain.profile.entity.Profile;
@@ -12,9 +14,16 @@ import com.meoguri.linkocean.domain.profile.entity.Profile;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 팔로우
+ * 팔로워 - 팔로이 조합은 유니크하다.
+ */
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 @Entity
+@Table(
+	uniqueConstraints = @UniqueConstraint(columnNames = {"follower_id", "followee_id"})
+)
 public class Follow extends BaseIdEntity {
 
 	@ManyToOne(fetch = LAZY)
