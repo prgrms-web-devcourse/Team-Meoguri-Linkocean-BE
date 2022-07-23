33b7e70e60264993890bad2067a4bd4c22cf6569
hyuk0309 <bahngeh@naver.com>
2022-07-23 20:47:33 +0900

    feat: category 엔티티 개발

---
+++ src/main/java/com/meoguri/linkocean/domain/category/entity/Category.java
@@ -0,0 +1,25 @@
+package com.meoguri.linkocean.domain.category.entity;
+
+import static lombok.AccessLevel.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import com.meoguri.linkocean.domain.common.BaseIdEntity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 북마크 카테고리
+ */
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
+public class Category extends BaseIdEntity {
+
+	public static final int CATEGORY_NAME_MAX_LENGTH = 50;
+
+	@Column(nullable = false, unique = true, length = CATEGORY_NAME_MAX_LENGTH)
+	private String name;
+}
