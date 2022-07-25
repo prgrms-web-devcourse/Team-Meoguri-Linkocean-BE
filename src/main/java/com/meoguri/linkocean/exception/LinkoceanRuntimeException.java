8cf623d1b4c1639ff8f80c6555f7ca780292f03d
ndy2 <emrdbs12@gmail.com>
2022-07-26 06:34:05 +0900

    chore: 주석변경

--- src/main/java/com/meoguri/linkocean/exception/LinkoceanRuntimeException.java
+++ src/main/java/com/meoguri/linkocean/exception/LinkoceanRuntimeException.java
@@ -2,7 +2,7 @@ package com.meoguri.linkocean.exception;
 
 /**
  * 버그 및 공격 상황 처리를 위한 커스텀 예외
- * e.g.) 없는 아이디로 조회를 요청하는 경우
+ * <br> e.g.) 없는 아이디로 조회를 요청하는 경우
  */
 public class LinkoceanRuntimeException extends RuntimeException {
 
