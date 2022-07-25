376221cf3ca449ab8c9df885481a61e9867e93aa
ndy2 <emrdbs12@gmail.com>
2022-07-26 06:28:36 +0900

    feat: 리액션 테이블 정보 변경
    - profileId 누락 된것 추가
    - profile-bookmark-type uniqueConstraint 추가

--- src/main/java/com/meoguri/linkocean/domain/bookmark/entity/Reaction.java
+++ src/main/java/com/meoguri/linkocean/domain/bookmark/entity/Reaction.java
@@ -7,8 +7,11 @@ import static lombok.AccessLevel.*;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.meoguri.linkocean.domain.BaseIdEntity;
+import com.meoguri.linkocean.domain.profile.entity.Profile;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +19,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 @Entity
+@Table(
+	uniqueConstraints = @UniqueConstraint(columnNames = {"profile_id", "bookmark_id", "type"})
+)
 public class Reaction extends BaseIdEntity {
+
+	@ManyToOne(fetch = LAZY)
+	private Profile profile;
 
 	@ManyToOne(fetch = LAZY)
 	private Bookmark bookmark;
@@ -24,15 +33,19 @@ public class Reaction extends BaseIdEntity {
 	@Enumerated(STRING)
 	private ReactionType type;
 
-	public Reaction(final Bookmark bookmark, final ReactionType type) {
+	public Reaction(final Profile profile, final Bookmark bookmark, final ReactionType type) {
 
+		this.profile = profile;
 		this.bookmark = bookmark;
 		this.type = type;
 	}
 
 	public enum ReactionType {
 
+		/* 좋아요 👍 */
 		LIKE,
+
+		/* 싫어요 👎 */
 		HATE
 	}
 }
