da7bb0b73cabbabe88a0b4a3a97e727e4d86cd18
ndy2 <emrdbs12@gmail.com>
2022-07-26 07:07:22 +0900

    feat: 리액션 추가/취소 서비스 계층

---
+++ src/main/java/com/meoguri/linkocean/domain/bookmark/repository/ReactionRepository.java
@@ -0,0 +1,13 @@
+package com.meoguri.linkocean.domain.bookmark.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.meoguri.linkocean.domain.bookmark.entity.Bookmark;
+import com.meoguri.linkocean.domain.bookmark.entity.Reaction;
+import com.meoguri.linkocean.domain.bookmark.entity.Reaction.ReactionType;
+import com.meoguri.linkocean.domain.profile.entity.Profile;
+
+public interface ReactionRepository extends JpaRepository<Reaction, Long> {
+
+	int deleteByProfileAndBookmarkAndType(Profile profile, Bookmark bookmark, ReactionType type);
+}
---
+++ src/main/java/com/meoguri/linkocean/domain/bookmark/service/dto/ReactionCommand.java
@@ -0,0 +1,21 @@
+package com.meoguri.linkocean.domain.bookmark.service.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 리액션을 추가/취소 하기 위한 커맨드
+ */
+@Getter
+@RequiredArgsConstructor
+public class ReactionCommand {
+
+	/* 리액션을 추가/취소 한 사용자의 아이디 */
+	private final long userId;
+
+	/* 리액션을 추가/취소 한 북마크의 아이디 */
+	private final long bookmarkId;
+
+	/* 리액션종류 */
+	private final String reactionType;
+}
