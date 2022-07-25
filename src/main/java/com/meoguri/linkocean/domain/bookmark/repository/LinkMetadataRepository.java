d6c99721fd78f959e4f32a3e23a43010c2e6f38d
hyuk0309 <bahngeh@naver.com>
2022-07-26 01:11:11 +0900

    feat(LinkMetadataRepository.java): 페이징 조회 API 개발

--- src/main/java/com/meoguri/linkocean/domain/bookmark/repository/LinkMetadataRepository.java
+++ src/main/java/com/meoguri/linkocean/domain/bookmark/repository/LinkMetadataRepository.java
@@ -2,6 +2,8 @@ package com.meoguri.linkocean.domain.bookmark.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,4 +16,7 @@ public interface LinkMetadataRepository extends JpaRepository<LinkMetadata, Long
 
 	@Query("select l.title from LinkMetadata l where l.url = :url")
 	Optional<String> findTitleByUrl(Url url);
+
+	/* 배치작업 최적화를 위해 Slice 이용 */
+	Slice<LinkMetadata> findBy(Pageable pageable);
 }
