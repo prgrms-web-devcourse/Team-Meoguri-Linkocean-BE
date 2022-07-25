202f7436264a122498528096b11e3df5796d30de
ndy2 <emrdbs12@gmail.com>
2022-07-26 06:31:28 +0900

    feat: 북마크 아이디로 조회하는 쿼리 개발

---
+++ src/main/java/com/meoguri/linkocean/domain/bookmark/service/GetBookmarkByIdQuery.java
@@ -0,0 +1,22 @@
+package com.meoguri.linkocean.domain.bookmark.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.meoguri.linkocean.domain.bookmark.entity.Bookmark;
+import com.meoguri.linkocean.domain.bookmark.repository.BookmarkRepository;
+import com.meoguri.linkocean.exception.LinkoceanRuntimeException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GetBookmarkByIdQuery {
+
+	private final BookmarkRepository bookmarkRepository;
+
+	public Bookmark GetById(final Long id) {
+		return bookmarkRepository.findById(id).orElseThrow(LinkoceanRuntimeException::new);
+	}
+}
