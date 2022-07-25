bdb300de4d5ef6ee1323159ddd50f61f542ce389
ndy2 <emrdbs12@gmail.com>
2022-07-25 19:34:56 +0900

    test: 선호 카테고리 추가 서비스 테스트 - 미완성
    TODO - 프로필 서비스 구현 이후 완성

---
+++ src/test/java/com/meoguri/linkocean/domain/category/service/FavoriteCategoryServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.meoguri.linkocean.domain.category.service;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.meoguri.linkocean.domain.category.service.dto.AddFavoriteCategoriesCommand;
+
+@Sql("classpath:sql/InsertCategories.sql")
+@Transactional
+@SpringBootTest(webEnvironment = NONE)
+class FavoriteCategoryServiceImplTest {
+
+	@Autowired
+	private FavoriteCategoryService favoriteCategoryService;
+
+	@BeforeEach
+	void setUp() {
+		//TODO - User, Profile 하나씩 추가
+	}
+
+	// TODO - 프로필 서비스 구현 이후 완성
+	@Test
+	void 선호_카테고리_추가_성공() {
+		//given
+		favoriteCategoryService.addFavoriteCategories(
+			new AddFavoriteCategoriesCommand(
+				1L,
+				List.of("자기계발", "인문")
+			)
+		);
+	}
+}
