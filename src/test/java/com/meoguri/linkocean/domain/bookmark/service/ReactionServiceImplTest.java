357445a1cb5de307e5520d193a0db9ed9fd3362b
ndy2 <emrdbs12@gmail.com>
2022-07-26 07:19:58 +0900

    chore: add new line in test

--- src/test/java/com/meoguri/linkocean/domain/bookmark/service/ReactionServiceImplTest.java
+++ src/test/java/com/meoguri/linkocean/domain/bookmark/service/ReactionServiceImplTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-@Disabled("Bookmark Service 가 등장하지 않은 시점에서 적절한 통합 테스트가 서비스 계층에서 제한되서 disabled")
+@Disabled("Bookmark Service 가 등장하지 않은 시점에서 적절한 통합 테스트가 서비스 계층에서 제한되서 disabled트")
 @Transactional
 @SpringBootTest
 class ReactionServiceImplTest {
--- src/test/java/com/meoguri/linkocean/domain/profile/service/GetProfileByUserIdQueryTest.java
+++ src/test/java/com/meoguri/linkocean/domain/profile/service/GetProfileByUserIdQueryTest.java
@@ -8,4 +8,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @SpringBootTest
 class GetProfileByUserIdQueryTest {
+
 }
