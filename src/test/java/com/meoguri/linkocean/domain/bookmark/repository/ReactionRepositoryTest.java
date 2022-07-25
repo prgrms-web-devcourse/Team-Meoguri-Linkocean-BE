13a35ab33e55f20890e73904fed4b5be96fb75d5
ndy2 <emrdbs12@gmail.com>
2022-07-26 07:07:52 +0900

    test: 리액션 추가/취소 서비스/영속성 계층 테스트

---
+++ src/test/java/com/meoguri/linkocean/domain/bookmark/repository/ReactionRepositoryTest.java
@@ -0,0 +1,89 @@
+package com.meoguri.linkocean.domain.bookmark.repository;
+
+import static com.meoguri.linkocean.domain.bookmark.entity.vo.OpenType.*;
+import static com.meoguri.linkocean.domain.util.Fixture.*;
+import static org.assertj.core.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import com.meoguri.linkocean.domain.bookmark.entity.Bookmark;
+import com.meoguri.linkocean.domain.bookmark.entity.Reaction;
+import com.meoguri.linkocean.domain.bookmark.entity.Reaction.ReactionType;
+import com.meoguri.linkocean.domain.profile.entity.Profile;
+import com.meoguri.linkocean.domain.profile.repository.ProfileRepository;
+import com.meoguri.linkocean.domain.user.repository.UserRepository;
+
+@DataJpaTest
+class ReactionRepositoryTest {
+
+	@Autowired
+	private EntityManager em;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private ProfileRepository profileRepository;
+
+	@Autowired
+	private BookmarkRepository bookmarkRepository;
+
+	@Autowired
+	private LinkMetadataRepository linkMetadataRepository;
+
+	@Autowired
+	private ReactionRepository reactionRepository;
+
+	private Profile profile;
+	private Bookmark bookmark;
+
+	@BeforeEach
+	void setUp() {
+		profile = profileRepository.save(new Profile(userRepository.save(createUser()), "haha"));
+		bookmark = bookmarkRepository.save(
+			Bookmark.builder()
+				.profile(profile)
+				.linkMetadata(linkMetadataRepository.save(createLinkMetadata()))
+				.title("title")
+				.memo("memo")
+				.openType(ALL)
+				.build());
+	}
+
+	@Test
+	void 프로필_북마크_리액션타입_조합은_유니크하다() {
+		//given
+		reactionRepository.save(new Reaction(profile, bookmark, ReactionType.LIKE));
+
+		//when then
+		assertThatExceptionOfType(DataIntegrityViolationException.class)
+			.isThrownBy(() -> reactionRepository.save(new Reaction(profile, bookmark, ReactionType.LIKE)));
+	}
+
+	@Test
+	void 프로필_북마크_리액션타입_조합으로_삭제할수_있다() {
+		//given
+		final Reaction reaction = reactionRepository.save(new Reaction(profile, bookmark, ReactionType.LIKE));
+		em.flush();
+		em.clear();
+
+		//when
+		final boolean isDeleted
+			= reactionRepository.deleteByProfileAndBookmarkAndType(profile, bookmark, ReactionType.LIKE) > 0;
+		em.flush(); // <- delete 쿼리 발생 어떻게 id 에 대한 in 절 쿼리가 나가는지 의문
+		em.clear();
+
+		//then
+		assertThat(isDeleted).isTrue();
+		assertThat(reactionRepository.findById(reaction.getId())).isEmpty();
+
+		em.flush();
+		em.clear();
+	}
+}
