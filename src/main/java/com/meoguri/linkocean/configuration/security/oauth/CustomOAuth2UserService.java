fb1035a32c0285d836111b202b23f016fe79a3af
ndy2 <emrdbs12@gmail.com>
2022-07-26 16:55:59 +0900

    refactor: attribute -> user 로직 리팩토링, 주석

--- src/main/java/com/meoguri/linkocean/configuration/security/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/meoguri/linkocean/configuration/security/oauth/CustomOAuth2UserService.java
@@ -18,6 +18,10 @@ import com.meoguri.linkocean.domain.user.repository.UserRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 커스텀 OAuth2 사용자 서비스
+ * - 테스트는 index.html 을 띄우고 눈으로 확인해 주세요.
+ */
 @Slf4j
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
@@ -32,6 +36,10 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 		this.delegate = new DefaultOAuth2UserService();
 	}
 
+	/**
+	 * delegate 를 통한 loadUser 수행 및 <br>
+	 * 유저를 생성 저장하고 세션에 저장하는 역할을 추가로 수행한다.
+	 */
 	@Override
 	public OAuth2User loadUser(final OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
@@ -39,7 +47,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 		final String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
 		final OAuthAttributes attributes = OAuthAttributes.of(registrationId, oAuth2User.getAttributes());
-		final User user = save(attributes);
+		final User user = userOf(attributes);
 
 		httpSession.setAttribute("user", new SessionUser(user));
 
@@ -54,11 +62,15 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 		);
 	}
 
-	private User save(final OAuthAttributes attributes) {
-		final User user = userRepository.findByEmail(new Email(attributes.getEmail()))
-			.orElse(attributes.toEntity());
+	private User userOf(final OAuthAttributes attributes) {
 
-		log.info("새로운 사용자 저장 email : {}, oauth type : {}", Email.toString(user.getEmail()), user.getOAuthType());
-		return userRepository.save(user);
+		//email 로 조회 후 이미 존재한다면 조회한 엔티티 반환
+		return userRepository.findByEmail(new Email(attributes.getEmail())).orElseGet(() -> {
+
+			//그렇지 않다면 새로운 사용자 생성후 저장
+			final User user = attributes.toEntity();
+			log.info("새로운 사용자 저장 email : {}, oauth type : {}", Email.toString(user.getEmail()), user.getOAuthType());
+			return userRepository.save(user);
+		});
 	}
 }
