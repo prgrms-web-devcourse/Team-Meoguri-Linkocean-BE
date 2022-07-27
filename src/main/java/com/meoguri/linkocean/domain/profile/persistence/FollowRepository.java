7dfb766e4b41c72eee0db859f21e2296ec7fb2a6
ndy2 <emrdbs12@gmail.com>
2022-07-27 23:03:15 +0900

    feat: 팔로우 리포지토리 구현, 테스트

--- src/main/java/com/meoguri/linkocean/domain/profile/persistence/FollowRepository.java
+++ src/main/java/com/meoguri/linkocean/domain/profile/persistence/FollowRepository.java
@@ -12,4 +12,16 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
 
 	@Query("select f from Follow f where f.follower = :follower and f. followee = :followee")
 	Optional<Follow> findByProfiles(Profile follower, Profile followee);
+
+	/**
+	 * user 를 팔로우 하는 사용자의 카운트
+	 */
+	@Query("select count(f) from Follow f join f.followee p join p.user u where u.id = :userId")
+	int countFollowerByUserId(long userId);
+
+	/**
+	 * user 가 팔로우 하는 사용자의 카운트
+	 */
+	@Query("select count(f) from Follow f join f.follower p join p.user u where u.id = :userId")
+	int countFolloweeByUserId(long userId);
 }
---
+++ src/test/java/com/meoguri/linkocean/domain/profile/persistence/FollowRepositoryTest.java
@@ -0,0 +1,82 @@
+package com.meoguri.linkocean.domain.profile.persistence;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.meoguri.linkocean.domain.profile.entity.Follow;
+import com.meoguri.linkocean.domain.profile.entity.Profile;
+import com.meoguri.linkocean.domain.user.entity.User;
+import com.meoguri.linkocean.domain.user.entity.vo.OAuthType;
+import com.meoguri.linkocean.domain.user.repository.UserRepository;
+
+@DataJpaTest
+class FollowRepositoryTest {
+
+	@Autowired
+	private FollowRepository followRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private ProfileRepository profileRepository;
+
+	User user1;
+	User user2;
+
+	Profile profile1;
+	Profile profile2;
+
+	@BeforeEach
+	void setUp() {
+		user1 = userRepository.save(new User("haha@gmail.com", OAuthType.GOOGLE));
+		user2 = userRepository.save(new User("papa@gmail.com", OAuthType.GOOGLE));
+
+		profile1 = profileRepository.save(new Profile(user1, "haha"));
+		profile2 = profileRepository.save(new Profile(user2, "papa"));
+	}
+
+	@Test
+	void 팔로워_팔로이_조합으로_조회_성공() {
+		//given
+		Profile follower = profile1;
+		Profile followee = profile2;
+		followRepository.save(new Follow(follower, followee));
+
+		//when
+		final Optional<Follow> oFollow = followRepository.findByProfiles(follower, followee);
+
+		//then
+		assertThat(oFollow).isPresent().get()
+			.extracting(Follow::getFollower, Follow::getFollowee)
+			.containsExactly(follower, followee);
+	}
+
+	@Test
+	void 팔로워_팔로이_카운트_성공() {
+		//given
+		Profile follower = profile1;
+		Profile followee = profile2;
+		followRepository.save(new Follow(follower, followee));
+
+		//when
+		int countProfile1Follower = followRepository.countFollowerByUserId(user1.getId());
+		int countProfile1Followee = followRepository.countFolloweeByUserId(user1.getId());
+
+		int countProfile2Follower = followRepository.countFollowerByUserId(user2.getId());
+		int countProfile2Followee = followRepository.countFolloweeByUserId(user2.getId());
+
+		//then
+		assertThat(countProfile1Follower).isEqualTo(0);
+		assertThat(countProfile1Followee).isEqualTo(1);
+
+		assertThat(countProfile2Follower).isEqualTo(1);
+		assertThat(countProfile2Followee).isEqualTo(0);
+	}
+}
