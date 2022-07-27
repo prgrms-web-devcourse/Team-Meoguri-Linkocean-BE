ed7c2c8e169a7c44c1e49ecd18805e76ecd2a80f
SeungHan <jk05018@naver.com>
2022-07-27 09:08:01 +0900

    refactor: 배포용 healthcheck url 추가
    단순 배포 확인용 IndexController 추후 삭제 예정

--- src/main/java/com/meoguri/linkocean/IndexController.java
+++ src/main/java/com/meoguri/linkocean/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-	@GetMapping
+	@GetMapping("/api/v1/health")
 	public String test() {
 		return "deploy success";
 	}
