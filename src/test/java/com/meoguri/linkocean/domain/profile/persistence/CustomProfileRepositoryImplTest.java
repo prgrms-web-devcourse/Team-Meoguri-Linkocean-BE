a2353bcc99f7c27f4c73219ee2983171df154aa1
ndy2 <emrdbs12@gmail.com>
2022-07-29 00:29:47 +0900

    test: 팔로워/팔로이 프로필 목록 조회 테스트

---
+++ src/test/java/com/meoguri/linkocean/domain/profile/persistence/CustomProfileRepositoryImplTest.java
@@ -0,0 +1,115 @@
+package com.meoguri.linkocean.domain.profile.persistence;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.meoguri.linkocean.common.QueryDslTest;
+import com.meoguri.linkocean.domain.profile.entity.Follow;
+import com.meoguri.linkocean.domain.profile.entity.Profile;
+import com.meoguri.linkocean.domain.profile.persistence.dto.FindProfileCond;
+import com.meoguri.linkocean.domain.user.entity.User;
+import com.meoguri.linkocean.domain.user.repository.UserRepository;
+
+@QueryDslTest
+class CustomProfileRepositoryImplTest {
+
+	@Autowired
+	private ProfileRepository profileRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private FollowRepository followRepository;
+
+	private Profile profile1;
+	private Profile profile2;
+	private Profile profile3;
+
+	@BeforeEach
+	void setUp() {
+		//set up 3 users
+		User user1 = userRepository.save(new User("user1@gmail.com", "GOOGLE"));
+		User user2 = userRepository.save(new User("user2@naver.com", "NAVER"));
+		User user3 = userRepository.save(new User("user3@kakao.com", "KAKAO"));
+
+		profile1 = profileRepository.save(new Profile(user1, "user1"));
+		profile2 = profileRepository.save(new Profile(user2, "user2"));
+		profile3 = profileRepository.save(new Profile(user3, "user3"));
+	}
+
+	@Test
+	void 팔로워_목록조회_성공_이름_지정_X() {
+		//given
+		followRepository.save(new Follow(profile1, profile2));
+		followRepository.save(new Follow(profile1, profile3));
+		followRepository.save(new Follow(profile2, profile3));
+
+		//when
+		final List<Profile> followerOfUser1 = profileRepository.findFollowerProfilesBy(findCond(profile1));
+		final List<Profile> followerOfUser2 = profileRepository.findFollowerProfilesBy(findCond(profile2));
+		final List<Profile> followerOfUser3 = profileRepository.findFollowerProfilesBy(findCond(profile3));
+
+		//then
+		assertThat(followerOfUser1).isEmpty();
+		assertThat(followerOfUser2).containsExactly(profile1);
+		assertThat(followerOfUser3).containsExactly(profile1, profile2);
+	}
+
+	@Test
+	void 팔로워_목록조회_성공_이름_지정() {
+		//given
+		followRepository.save(new Follow(profile1, profile3));
+		followRepository.save(new Follow(profile2, profile3));
+
+		//when
+		final List<Profile> followerOfUser1 = profileRepository.findFollowerProfilesBy(findCond(profile3, "user1"));
+
+		//then
+		assertThat(followerOfUser1).containsExactly(profile1);
+	}
+
+	@Test
+	void 팔로이_목록조회_성공_이름_지정_X() {
+		//given
+		followRepository.save(new Follow(profile1, profile2));
+		followRepository.save(new Follow(profile1, profile3));
+		followRepository.save(new Follow(profile2, profile3));
+
+		//when
+		final List<Profile> followerOfUser1 = profileRepository.findFolloweeProfilesBy(findCond(profile1));
+		final List<Profile> followerOfUser2 = profileRepository.findFolloweeProfilesBy(findCond(profile2));
+		final List<Profile> followerOfUser3 = profileRepository.findFolloweeProfilesBy(findCond(profile3));
+
+		//then
+		assertThat(followerOfUser1).containsExactly(profile2, profile3);
+		assertThat(followerOfUser2).containsExactly(profile3);
+		assertThat(followerOfUser3).isEmpty();
+	}
+
+	@Test
+	void 팔로이_목록조회_성공_이름_지정() {
+		//given
+		followRepository.save(new Follow(profile1, profile2));
+		followRepository.save(new Follow(profile1, profile3));
+
+		//when
+		final List<Profile> followerOfUser1 = profileRepository.findFolloweeProfilesBy(findCond(profile1, "user3"));
+
+		//then
+		assertThat(followerOfUser1).containsExactly(profile3);
+	}
+
+	private FindProfileCond findCond(final Profile profile, final String username) {
+		return new FindProfileCond(profile.getId(), 1, 10, username);
+	}
+
+	private FindProfileCond findCond(final Profile profile) {
+		return findCond(profile, null);
+	}
+}
