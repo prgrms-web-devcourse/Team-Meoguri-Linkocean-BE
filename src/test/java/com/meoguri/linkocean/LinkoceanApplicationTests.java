e36b74cf882753089e6e211e43712420748a02a6
hyuk0309 <bahngeh@naver.com>
2022-07-28 22:17:30 +0900

    test(LinkoceanApplicationTests) : contextLoad 테스트는 local 프로파일에서 실행되게 수정

--- src/test/java/com/meoguri/linkocean/LinkoceanApplicationTests.java
+++ src/test/java/com/meoguri/linkocean/LinkoceanApplicationTests.java
@@ -2,8 +2,10 @@ package com.meoguri.linkocean;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class LinkoceanApplicationTests {
 
 	@Test
