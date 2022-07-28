71cd222b05410cb0e6bc37dad06ceebce34db5b3
SeungHan <jk05018@naver.com>
2022-07-28 16:12:18 +0900

    feat: securityConfig 기본설정 일단 PermitAll

--- src/main/java/com/meoguri/linkocean/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/meoguri/linkocean/configuration/security/SecurityConfiguration.java
@@ -1,16 +1,16 @@
 package com.meoguri.linkocean.configuration.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.meoguri.linkocean.configuration.security.oauth.CustomOAuth2UserService;
 
 import lombok.RequiredArgsConstructor;
 
+@Configuration
 @RequiredArgsConstructor
-@EnableWebSecurity
 public class SecurityConfiguration {
 
 	private final CustomOAuth2UserService customOAuth2UserService;
@@ -18,6 +18,15 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
+			.csrf().disable()
+			.headers().frameOptions().disable()
+			.and()
+			.authorizeRequests()
+			.anyRequest().permitAll()
+			.and()
+			.logout()
+			.logoutSuccessUrl("/")
+			.and()
 			.oauth2Login(oauth2 -> oauth2.userInfoEndpoint().userService(customOAuth2UserService))
 			.build();
 	}
