cf58163a73e7a2fa8558fc4a5907d2c84f6e3070
ndy2 <emrdbs12@gmail.com>
2022-07-29 17:50:19 +0900

    test: 팔로워/팔로이 조회 서비스 마무리, 테스트

--- src/test/java/com/meoguri/linkocean/domain/profile/service/ProfileServiceImplTest.java
+++ src/test/java/com/meoguri/linkocean/domain/profile/service/ProfileServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.meoguri.linkocean.domain.profile.service;
 
 import static com.meoguri.linkocean.domain.util.Fixture.*;
+import static java.util.Collections.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
@@ -9,14 +10,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.meoguri.linkocean.domain.profile.entity.Profile;
+import com.meoguri.linkocean.domain.profile.service.dto.FollowCommand;
 import com.meoguri.linkocean.domain.profile.service.dto.GetMyProfileResult;
+import com.meoguri.linkocean.domain.profile.service.dto.ProfileSearchCond;
 import com.meoguri.linkocean.domain.profile.service.dto.RegisterProfileCommand;
+import com.meoguri.linkocean.domain.profile.service.dto.SearchProfileResult;
 import com.meoguri.linkocean.domain.profile.service.dto.UpdateProfileCommand;
 import com.meoguri.linkocean.domain.user.entity.User;
 import com.meoguri.linkocean.domain.user.repository.UserRepository;
@@ -34,102 +39,230 @@ class ProfileServiceImplTest {
 	@Autowired
 	private ProfileService profileService;
 
-	private long userId;
+	@Nested
+	class 프로필_등록_수정_조회_테스트 {
 
-	private Profile profile;
+		private long userId;
 
-	private List<String> categories;
+		private Profile profile;
 
-	@BeforeEach
-	void setUp() {
-		User user = userRepository.save(createUser());
-		userId = user.getId();
+		private List<String> categories;
 
-		profile = createProfile(user);
-		categories = List.of("it", "self_development");
-	}
+		@BeforeEach
+		void setUp() {
+			User user = userRepository.save(createUser());
+			userId = user.getId();
 
-	@Test
-	void 프로필_등록하고_조회_성공() {
-		//given
-		final RegisterProfileCommand command = ProfileCommand.ofRegister(profile, categories);
-		final long profileId = profileService.registerProfile(command);
-
-		em.flush();
-		em.clear();
-
-		//when
-		final GetMyProfileResult result = profileService.getMyProfile(userId);
-
-		//then
-		assertThat(result).extracting(
-			GetMyProfileResult::getProfileId,
-			GetMyProfileResult::getUsername,
-			GetMyProfileResult::getImageUrl,
-			GetMyProfileResult::getBio,
-			GetMyProfileResult::getFollowerCount,
-			GetMyProfileResult::getFolloweeCount,
-			GetMyProfileResult::isFollow
-		).containsExactly(
-			profileId,
-			profile.getUsername(),
-			profile.getImageUrl(),
-			profile.getBio(),
-			0,
-			0,
-			false
-		);
-
-		assertThat(result.getCategories()).containsExactly("it", "self_development");
-	}
-
-	@Test
-	void 프로필_등록하고_수정하고_조회_성공() {
-		//given
-		final RegisterProfileCommand registerCommand = ProfileCommand.ofRegister(profile, categories);
-		profileService.registerProfile(registerCommand);
-
-		em.flush();
-		em.clear();
-
-		//when
-		final UpdateProfileCommand updateCommand = new UpdateProfileCommand(
-			userId,
-			"papa",
-			"updated image url",
-			"updated bio",
-			List.of("it", "science")
-		);
-
-		profileService.updateProfile(updateCommand);
-
-		em.flush();
-		em.clear();
-
-		//then
-		final GetMyProfileResult result = profileService.getMyProfile(userId);
-		assertThat(result).extracting(
-			GetMyProfileResult::getUsername,
-			GetMyProfileResult::getImageUrl,
-			GetMyProfileResult::getBio
-		).containsExactly(
-			"papa",
-			"updated image url",
-			"updated bio"
-		);
-
-		assertThat(result.getCategories()).containsExactly("it", "science");
-	}
-
-	static final class ProfileCommand {
-
-		public static RegisterProfileCommand ofRegister(Profile profile, List<String> categories) {
-
-			return new RegisterProfileCommand(
-				profile.getUser().getId(),
-				profile.getUsername(),
-				categories
-			);
+			profile = createProfile(user);
+			categories = List.of("it", "self_development");
 		}
+
+		@Test
+		void 프로필_등록하고_조회_성공() {
+			//given
+			final RegisterProfileCommand command = RegisterCommandOf(profile, categories);
+			final long profileId = profileService.registerProfile(command);
+
+			em.flush();
+			em.clear();
+
+			//when
+			final GetMyProfileResult result = profileService.getMyProfile(userId);
+
+			//then
+			assertThat(result).extracting(
+				GetMyProfileResult::getProfileId,
+				GetMyProfileResult::getUsername,
+				GetMyProfileResult::getImageUrl,
+				GetMyProfileResult::getBio,
+				GetMyProfileResult::getFollowerCount,
+				GetMyProfileResult::getFolloweeCount,
+				GetMyProfileResult::isFollow
+			).containsExactly(
+				profileId,
+				profile.getUsername(),
+				profile.getImageUrl(),
+				profile.getBio(),
+				0,
+				0,
+				false
+			);
+
+			assertThat(result.getCategories()).containsExactly("it", "self_development");
+		}
+
+		@Test
+		void 프로필_등록하고_수정하고_조회_성공() {
+			//given
+			final RegisterProfileCommand registerCommand = RegisterCommandOf(profile, categories);
+			profileService.registerProfile(registerCommand);
+
+			em.flush();
+			em.clear();
+
+			//when
+			final UpdateProfileCommand updateCommand = new UpdateProfileCommand(
+				userId,
+				"papa",
+				"updated image url",
+				"updated bio",
+				List.of("it", "science")
+			);
+
+			profileService.updateProfile(updateCommand);
+
+			em.flush();
+			em.clear();
+
+			//then
+			final GetMyProfileResult result = profileService.getMyProfile(userId);
+			assertThat(result).extracting(
+				GetMyProfileResult::getUsername,
+				GetMyProfileResult::getImageUrl,
+				GetMyProfileResult::getBio
+			).containsExactly(
+				"papa",
+				"updated image url",
+				"updated bio"
+			);
+
+			assertThat(result.getCategories()).containsExactly("it", "science");
+		}
+	}
+
+	@Nested
+	class 프로필_목록_조회_테스트 {
+
+		private long user1Id;
+		private long user2Id;
+		private long user3Id;
+
+		private long profile1Id;
+		private long profile2Id;
+		private long profile3Id;
+
+		@Autowired
+		private FollowService followService;
+
+		@BeforeEach
+		void setUp() {
+			// set up 3 users
+			User user1 = userRepository.save(new User("user1@gmail.com", "GOOGLE"));
+			User user2 = userRepository.save(new User("user2@naver.com", "NAVER"));
+			User user3 = userRepository.save(new User("user3@kakao.com", "KAKAO"));
+
+			user1Id = user1.getId();
+			user2Id = user2.getId();
+			user3Id = user3.getId();
+
+			Profile profile1 = new Profile(user1, "user1");
+			Profile profile2 = new Profile(user2, "user2");
+			Profile profile3 = new Profile(user3, "user3");
+
+			profile1Id = profileService.registerProfile(RegisterCommandOf(profile1, emptyList()));
+			profile2Id = profileService.registerProfile(RegisterCommandOf(profile2, emptyList()));
+			profile3Id = profileService.registerProfile(RegisterCommandOf(profile3, emptyList()));
+		}
+
+		@Test
+		void 팔로워_목록_조회_성공() {
+			//given
+			followService.follow(new FollowCommand(user1Id, profile2Id));
+			followService.follow(new FollowCommand(user1Id, profile3Id));
+			followService.follow(new FollowCommand(user2Id, profile3Id));
+			followService.follow(new FollowCommand(user3Id, profile2Id));
+
+			//when
+			final List<SearchProfileResult> result1
+				= profileService.searchFollowerProfiles(defaultSearchCondOfUserId(user1Id));
+			final List<SearchProfileResult> result2
+				= profileService.searchFollowerProfiles(defaultSearchCondOfUserId(user2Id));
+			final List<SearchProfileResult> result3
+				= profileService.searchFollowerProfiles(defaultSearchCondOfUserId(user3Id));
+
+			//then
+			assertThat(result1).isEmpty();
+			assertThat(result2)
+				.extracting(
+					SearchProfileResult::getId,
+					SearchProfileResult::getUsername,
+					SearchProfileResult::getImageUrl,
+					SearchProfileResult::isFollow
+				).containsExactly(
+					tuple(user1Id, "user1", null, false),
+					tuple(user3Id, "user3", null, true)
+				);
+			assertThat(result3)
+				.extracting(
+					SearchProfileResult::getId,
+					SearchProfileResult::getUsername,
+					SearchProfileResult::getImageUrl,
+					SearchProfileResult::isFollow
+				).containsExactly(
+					tuple(user1Id, "user1", null, false),
+					tuple(user2Id, "user2", null, true)
+				);
+		}
+
+		@Test
+		void 팔로이_목록_조회_성공() {
+			//given
+			followService.follow(new FollowCommand(user1Id, profile2Id));
+			followService.follow(new FollowCommand(user1Id, profile3Id));
+			followService.follow(new FollowCommand(user2Id, profile3Id));
+			followService.follow(new FollowCommand(user3Id, profile2Id));
+
+			//when
+			final List<SearchProfileResult> result1
+				= profileService.searchFolloweeProfiles(defaultSearchCondOfUserId(user1Id));
+			final List<SearchProfileResult> result2
+				= profileService.searchFolloweeProfiles(defaultSearchCondOfUserId(user2Id));
+			final List<SearchProfileResult> result3
+				= profileService.searchFolloweeProfiles(defaultSearchCondOfUserId(user3Id));
+
+			//then
+			assertThat(result1)
+				.extracting(
+					SearchProfileResult::getId,
+					SearchProfileResult::getUsername,
+					SearchProfileResult::getImageUrl,
+					SearchProfileResult::isFollow
+				).containsExactly(
+					tuple(user2Id, "user2", null, true),
+					tuple(user3Id, "user3", null, true)
+				);
+			assertThat(result2)
+				.extracting(
+					SearchProfileResult::getId,
+					SearchProfileResult::getUsername,
+					SearchProfileResult::getImageUrl,
+					SearchProfileResult::isFollow
+				).containsExactly(
+					tuple(user3Id, "user3", null, true)
+				);
+			assertThat(result3)
+				.extracting(
+					SearchProfileResult::getId,
+					SearchProfileResult::getUsername,
+					SearchProfileResult::getImageUrl,
+					SearchProfileResult::isFollow
+				).containsExactly(
+					tuple(user2Id, "user2", null, true)
+				);
+		}
+	}
+
+	static RegisterProfileCommand RegisterCommandOf(Profile profile, List<String> categories) {
+
+		return new RegisterProfileCommand(
+			profile.getUser().getId(),
+			profile.getUsername(),
+			categories
+		);
+	}
+
+	static ProfileSearchCond defaultSearchCondOfUserId(long userId) {
+
+		return new ProfileSearchCond(userId, null, null, null);
 	}
 }
