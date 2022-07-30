aeb2b2775a292af3012b59634ee90d7ff6574bb8
ndy2 <emrdbs12@gmail.com>
2022-07-30 11:58:48 +0900

    test: 페이보릿 생성 테스트

---
+++ src/test/java/com/meoguri/linkocean/domain/bookmark/entity/FavoriteTest.java
@@ -0,0 +1,26 @@
+package com.meoguri.linkocean.domain.bookmark.entity;
+
+import static com.meoguri.linkocean.domain.util.Fixture.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.meoguri.linkocean.domain.profile.entity.Profile;
+
+class FavoriteTest {
+
+	@Test
+	void 페이보릿_생성_성공() {
+		//given
+		final Bookmark bookmark = createBookmark();
+		final Profile owner = createProfile();
+
+		//when
+		final Favorite favorite = new Favorite(bookmark, owner);
+
+		//then
+		assertThat(favorite).isNotNull()
+			.extracting(Favorite::getBookmark, Favorite::getOwner)
+			.containsExactly(bookmark, owner);
+	}
+}
