9ef8d2b5d98654d5af79dddce5b53f6f6f0d6674
ndy2 <emrdbs12@gmail.com>
2022-07-30 11:57:31 +0900

    migrate: add primary key

--- src/main/resources/db/migration/V011__Create_favorite.sql
+++ src/main/resources/db/migration/V011__Create_favorite.sql
@@ -2,5 +2,6 @@ create table if not exists favorite
 (
     id          bigint not null auto_increment,
     bookmark_id bigint,
-    owner_id    bigint
+    owner_id    bigint,
+    primary key (id)
 ) engine = InnoDB;
--- src/main/resources/db/migration/V013__Add_Foreign_Key_Contraints.sql
+++ src/main/resources/db/migration/V013__Add_Foreign_Key_Contraints.sql
@@ -1,28 +1,41 @@
 alter table bookmark
     add constraint fk_bookmark_link_metadata foreign key (link_metadata_id) references link_metadata (id);
+
 alter table bookmark
     add constraint fk_bookmark_profile foreign key (profile_id) references profile (id);
+
 alter table bookmark_tag
     add constraint fk_bookmarkTag_bookmark foreign key (bookmark_id) references bookmark (id);
+
 alter table bookmark_tag
     add constraint fk_bookmarkTag_tag foreign key (tag_id) references tag (id);
+
 alter table favorite_category
     add constraint fk_favoriteCategory_profile foreign key (profile_id) references profile (id);
+
 alter table follow
     add constraint fk_follow_followee foreign key (followee_id) references profile (id);
+
 alter table follow
     add constraint fk_follow_follower foreign key (follower_id) references profile (id);
+
 alter table notification
     add constraint fk_notification_bookmark foreign key (bookmark_id) references bookmark (id);
+
 alter table notification
     add constraint fk_notification_target foreign key (target_id) references profile (id);
+
 alter table profile
     add constraint fk_profile_users foreign key (user_id) references users (id);
+
 alter table reaction
     add constraint fk_reaction_bookmark foreign key (bookmark_id) references bookmark (id);
+
 alter table reaction
     add constraint fk_reaction_profile foreign key (profile_id) references profile (id);
+
 alter table favorite
-    add constraint fk_favorite_bookmark foreign key (boomark_id) references bookmark (id);
+    add constraint fk_favorite_bookmark foreign key (bookmark_id) references bookmark (id);
+
 alter table favorite
     add constraint fk_favorite_owner foreign key (owner_id) references profile (id);
