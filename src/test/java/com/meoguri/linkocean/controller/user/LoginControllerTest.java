03f87c753faef61ef013bcaac0915a8f055e6bd5
SeungHan <jk05018@naver.com>
2022-07-31 09:42:08 +0900

    refactor: security config 수정
    1. 소셜 로그인을 완료한 사람은 기본적으로 ROLE_USER 권한을 가짐
    2. SessionUser에 Serialize implementation
    3. LoginCOntrollerTest의 내용은 사용자가 프로필 등록이 되었는지 안되었는지 확인하는 용도, 따라서 통합테스트처럼 테스트할려면 프로필 등록 API가 작성되어 있어야함,
    -> 오늘 제가 작성하고 이 테스트까지 마저 작성하겠습니다.

--- src/test/java/com/meoguri/linkocean/controller/user/LoginControllerTest.java
+++ src/test/java/com/meoguri/linkocean/controller/user/LoginControllerTest.java
@@ -2,7 +2,7 @@ package com.meoguri.linkocean.controller.user;
 
 import com.meoguri.linkocean.controller.BaseControllerTest;
 
-//TODO - 로그인 통합 테스트 구현
+//TODO - 로그인 통합 테스트 구현 -> 프로필 API를 만든 다음 작성하겠습니다 - by hani
 class LoginControllerTest extends BaseControllerTest {
 
 }
