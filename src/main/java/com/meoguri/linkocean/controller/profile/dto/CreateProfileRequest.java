13acb3448c19d5d5208b5fe359f5a9a9c6967325
SeungHan <jk05018@naver.com>
2022-07-31 16:36:11 +0900

    feat: profile 등록 API 개발 완료

---
+++ src/main/java/com/meoguri/linkocean/controller/profile/dto/CreateProfileRequest.java
@@ -0,0 +1,21 @@
+package com.meoguri.linkocean.controller.profile.dto;
+
+import java.util.List;
+
+import com.meoguri.linkocean.domain.profile.service.dto.RegisterProfileCommand;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateProfileRequest {
+
+	private String username;
+	private List<String> categories;
+
+	public RegisterProfileCommand toCommand(final long userId) {
+		return new RegisterProfileCommand(userId, username, categories);
+	}
+
+}
