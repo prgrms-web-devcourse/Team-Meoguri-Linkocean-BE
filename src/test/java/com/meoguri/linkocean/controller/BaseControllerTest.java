81dde41a8a1e087f27dfcbdb065a01c4f59bcb35
SeungHan <jk05018@naver.com>
2022-07-31 16:35:54 +0900

    feat: profile 등록 API 테스트 작성
    1. BaseController에 유저_등록_로그인 protected
    2. createProfileTest 작성

--- src/test/java/com/meoguri/linkocean/controller/BaseControllerTest.java
+++ src/test/java/com/meoguri/linkocean/controller/BaseControllerTest.java
@@ -8,12 +8,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.meoguri.linkocean.common.P6spyLogMessageFormatConfiguration;
+import com.meoguri.linkocean.configuration.security.oauth.SessionUser;
+import com.meoguri.linkocean.domain.user.entity.User;
+import com.meoguri.linkocean.domain.user.repository.UserRepository;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -25,6 +29,18 @@ public class BaseControllerTest {
 
 	@Autowired
 	protected ObjectMapper objectMapper;
+
+	protected MockHttpSession session;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	protected void 유저_등록_로그인(final String email, final String oAuthType) {
+		final User savedUser = userRepository.save(new User(email, oAuthType));
+
+		session = new MockHttpSession();
+		session.setAttribute("user", new SessionUser(savedUser));
+	}
 
 	protected String createJson(Object dto) throws JsonProcessingException {
 		return objectMapper.writeValueAsString(dto);
---
+++ src/test/java/com/meoguri/linkocean/controller/profile/ProfileControllerTest.java
@@ -0,0 +1,42 @@
+package com.meoguri.linkocean.controller.profile;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.meoguri.linkocean.controller.BaseControllerTest;
+import com.meoguri.linkocean.controller.profile.dto.CreateProfileRequest;
+
+@Transactional
+class ProfileControllerTest extends BaseControllerTest {
+
+	private final String basePath = getBaseUrl(ProfileController.class);
+
+	@WithMockUser(roles = "USER")
+	@Test
+	void 프로필_등록_Api_성공() throws Exception {
+		//given
+		유저_등록_로그인("hani@gmail.com", "GOOGLE");
+
+		final String username = "hani";
+		final List<String> categories = List.of("ART", "SOCIAL", "IT");
+
+		final CreateProfileRequest createProfileRequest = new CreateProfileRequest(username, categories);
+
+		//when
+		mockMvc.perform(post(basePath).session(session)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(createProfileRequest)))
+
+			//then
+			.andExpect(status().isOk())
+			.andDo(print());
+	}
+}
