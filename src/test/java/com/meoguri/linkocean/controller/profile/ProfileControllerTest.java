af7a33cbedf5d43034a69cc61ad827f2475e97bc
SeungHan <jk05018@naver.com>
2022-08-01 18:32:15 +0900

    feat: 내 프로필 조회 Controller & test 작성

--- src/test/java/com/meoguri/linkocean/controller/profile/ProfileControllerTest.java
+++ src/test/java/com/meoguri/linkocean/controller/profile/ProfileControllerTest.java
@@ -1,5 +1,6 @@
 package com.meoguri.linkocean.controller.profile;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -9,12 +10,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.meoguri.linkocean.controller.BaseControllerTest;
 import com.meoguri.linkocean.controller.profile.dto.CreateProfileRequest;
 
-@Transactional
 class ProfileControllerTest extends BaseControllerTest {
 
 	private final String basePath = getBaseUrl(ProfileController.class);
@@ -26,7 +25,7 @@ class ProfileControllerTest extends BaseControllerTest {
 		유저_등록_로그인("hani@gmail.com", "GOOGLE");
 
 		final String username = "hani";
-		final List<String> categories = List.of("ART", "SOCIAL", "IT");
+		final List<String> categories = List.of("인문", "정치", "사회");
 
 		final CreateProfileRequest createProfileRequest = new CreateProfileRequest(username, categories);
 
@@ -37,6 +36,45 @@ class ProfileControllerTest extends BaseControllerTest {
 
 			//then
 			.andExpect(status().isOk())
+			.andDo(print());
+	}
+
+	/*
+	저희 내 프로필 조회에서 내 Tag 목록과 내가 작성한 카테고리 목록 모두 작성하기로 했습니다.
+
+	아래의 내용들은 아직 API가 개발되지 않아서 추후 테스트를 보강하도록 하겠습니다.
+	1. 내가 작성한 카터고리 목록 - 완료
+	2. 내 Tag 목록
+	3. 팔로워, 팔로위 수
+	4. Bio, imageUrl
+	 */
+	@WithMockUser(roles = "USER")
+	@Test
+	void 내프로필_조회_Api_성공() throws Exception {
+		//given
+		유저_등록_로그인("hani@gmail.com", "GOOGLE");
+		프로필_등록("hani", List.of("인문", "정치", "사회"));
+		final String url = 링크_메타데이터_조회("http://www.naver.com");
+		북마크_등록(url, "인문", null, "private");
+
+		//when
+		mockMvc.perform(get(basePath + "/me").session(session)
+				.contentType(MediaType.APPLICATION_JSON))
+
+			//then
+			.andExpect(status().isOk())
+			.andExpectAll(
+				jsonPath("$.profileId").exists(),
+				jsonPath("$.imageUrl").isEmpty(),
+				jsonPath("$.favoriteCategories", hasSize(3)),
+				jsonPath("$.username").value("hani"),
+				jsonPath("$.bio").isEmpty(),
+				jsonPath("$.followerCount").value(0),
+				jsonPath("$.followeeCount").value(0),
+				jsonPath("$.tags", hasSize(0)),
+				jsonPath("$.categories", hasSize(1))
+
+			)
 			.andDo(print());
 	}
 }
