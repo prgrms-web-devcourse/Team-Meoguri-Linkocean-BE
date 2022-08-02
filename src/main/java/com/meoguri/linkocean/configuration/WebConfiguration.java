39de393a41b5641d84fbf22d8f58c39db6d1b527
SeungHan <jk05018@naver.com>
2022-08-02 15:13:53 +0900

    fix: cors 설정 수정

--- src/main/java/com/meoguri/linkocean/configuration/WebConfiguration.java
+++ src/main/java/com/meoguri/linkocean/configuration/WebConfiguration.java
@@ -2,12 +2,9 @@ package com.meoguri.linkocean.configuration;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.meoguri.linkocean.configuration.security.oauth.LoginUserArgumentResolver;
@@ -25,17 +22,11 @@ public class WebConfiguration implements WebMvcConfigurer {
 		resolvers.add(loginUserArgumentResolver);
 	}
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		// - (3)
-		configuration.addAllowedOrigin("*");
-		configuration.addAllowedMethod("*");
-		configuration.addAllowedHeader("*");
-		configuration.setAllowCredentials(true);
-		configuration.setMaxAge(3600L);
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST")
+			.maxAge(3000);
 	}
 }
