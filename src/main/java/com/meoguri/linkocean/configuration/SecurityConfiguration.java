798900cb777408c770cbe3ca160477f325e466c2
SeungHan <jk05018@naver.com>
2022-08-02 14:58:56 +0900

    fix: defaultSuccess 주석

--- src/main/java/com/meoguri/linkocean/configuration/SecurityConfiguration.java
+++ src/main/java/com/meoguri/linkocean/configuration/SecurityConfiguration.java
@@ -35,9 +35,9 @@ public class SecurityConfiguration {
 			.authenticationEntryPoint(customAuthenticationEntryPoint)
 			.and()
 			.oauth2Login(oauth2 ->
-				oauth2.userInfoEndpoint().userService(customOAuth2UserService)
-					.and()
-					.defaultSuccessUrl("/api/v1/login/success")
+					oauth2.userInfoEndpoint().userService(customOAuth2UserService)
+				// .and()
+				// .defaultSuccessUrl("/api/v1/login/success")
 			)
 			.build();
 	}
