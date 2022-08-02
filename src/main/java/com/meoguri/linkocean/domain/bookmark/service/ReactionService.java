180e00275defd103cb2076c4985f04f6e35eb16a
Juhan <juhan211@naver.com>
2022-08-02 16:07:04 +0900

    feature: Reaction Service 인터페이스 변경

--- src/main/java/com/meoguri/linkocean/domain/bookmark/service/ReactionService.java
+++ src/main/java/com/meoguri/linkocean/domain/bookmark/service/ReactionService.java
@@ -3,17 +3,5 @@ package com.meoguri.linkocean.domain.bookmark.service;
 import com.meoguri.linkocean.domain.bookmark.service.dto.ReactionCommand;
 
 public interface ReactionService {
-
-	/**
-	 * 리액션 추가
-	 * @throws javax.persistence.PersistenceException 중복 추가
-	 * @throws com.meoguri.linkocean.exception.LinkoceanRuntimeException 예외적인 요청
-	 */
-	void addReaction(ReactionCommand addReactionCommand);
-
-	/**
-	 * 리액션 취소
-	 * @throws com.meoguri.linkocean.exception.LinkoceanRuntimeException 예외적인 요청
-	 */
-	void cancelReaction(ReactionCommand reactionCommand);
+	void requestReaction(ReactionCommand command);
 }
