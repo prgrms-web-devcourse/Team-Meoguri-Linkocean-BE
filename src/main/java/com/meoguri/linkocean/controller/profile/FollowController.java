a92ff9d602a8b89abc3ff213c3afbd4cdfb54c53
ndy2 <emrdbs12@gmail.com>
2022-08-04 01:23:06 +0900

    feat: add FollowController

---
+++ src/main/java/com/meoguri/linkocean/controller/profile/FollowController.java
@@ -0,0 +1,36 @@
+package com.meoguri.linkocean.controller.profile;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.meoguri.linkocean.configuration.security.oauth.LoginUser;
+import com.meoguri.linkocean.configuration.security.oauth.SessionUser;
+import com.meoguri.linkocean.domain.profile.service.FollowService;
+import com.meoguri.linkocean.domain.profile.service.dto.FollowCommand;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/profiles/")
+@RestController
+public class FollowController {
+
+	private final FollowService followService;
+
+	@PostMapping("/follow")
+	public void follow(
+		@LoginUser SessionUser user,
+		long followeeId
+	) {
+		followService.follow(new FollowCommand(user.getId(), followeeId));
+	}
+
+	@PostMapping("/unfollow")
+	public void unfollow(
+		@LoginUser SessionUser user,
+		long followeeId
+	) {
+		followService.unfollow(new FollowCommand(user.getId(), followeeId));
+	}
+}
