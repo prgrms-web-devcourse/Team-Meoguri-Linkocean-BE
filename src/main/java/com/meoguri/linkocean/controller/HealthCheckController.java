4eb585abf7988b23bf14f10f6c10957e2a6507fd
SeungHan <jk05018@naver.com>
2022-08-03 14:51:32 +0900

    refactor: healthCheck url 수정

--- src/main/java/com/meoguri/linkocean/controller/HealthCheckController.java
+++ src/main/java/com/meoguri/linkocean/controller/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/healthCheck")
+@RequestMapping("/api/v1/healthCheck")
 public class HealthCheckController {
 
 	@GetMapping
