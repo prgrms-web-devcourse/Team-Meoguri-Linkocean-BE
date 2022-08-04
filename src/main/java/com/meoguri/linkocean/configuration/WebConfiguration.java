f83a5d51dbaa8839a7dbc15da56b42766782a30a
SeungHan <jk05018@naver.com>
2022-08-04 12:58:20 +0900

    fix: edit cors config

--- src/main/java/com/meoguri/linkocean/configuration/WebConfiguration.java
+++ src/main/java/com/meoguri/linkocean/configuration/WebConfiguration.java
@@ -28,8 +28,9 @@ public class WebConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(final CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
-			.allowedMethods("GET", "POST");
+			.allowedOrigins("http://localhost:8080", "http://localhost:3000")
+			.allowedMethods("GET", "POST")
+			.allowCredentials(true); //쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
 	}
 
 }
