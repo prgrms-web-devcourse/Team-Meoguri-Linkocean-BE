18f7c73e5299143ab52ef6c8361397851285a8db
SeungHan <jk05018@naver.com>
2022-08-04 18:23:44 +0900

    refactor: 세션 관련 클래스 제거

--- src/main/java/com/meoguri/linkocean/configuration/WebConfiguration.java
+++ src/main/java/com/meoguri/linkocean/configuration/WebConfiguration.java
@@ -2,15 +2,12 @@ package com.meoguri.linkocean.configuration;
 
 import java.util.List;
 
-import org.springframework.boot.web.server.Cookie;
-import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.meoguri.linkocean.configuration.security.oauth.LoginUserArgumentResolver;
 import com.meoguri.linkocean.controller.bookmark.support.GetBookmarkQueryParamsArgumentResolver;
 
 import lombok.RequiredArgsConstructor;
@@ -18,13 +15,10 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfiguration implements WebMvcConfigurer {
-
-	private final LoginUserArgumentResolver loginUserArgumentResolver;
 	private final GetBookmarkQueryParamsArgumentResolver getBookmarkQueryParamsArgumentResolver;
 
 	@Override
 	public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(loginUserArgumentResolver);
 		resolvers.add(getBookmarkQueryParamsArgumentResolver);
 	}
 
@@ -36,22 +30,8 @@ public class WebConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(final CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:8080", "http://localhost:3000")
-			.allowedMethods("GET", "POST")
-			.allowCredentials(true); //쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
-	}
-
-/*	@Bean
-	public CookieSerializer cookieSerializer() throws MalformedURLException {
-		DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-		serializer.setCookieName("JSESSIONID");
-		serializer.setSameSite("");
-		return serializer;
-	}*/
-
-	// @Bean
-	public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
-		return CookieSameSiteSupplier.of(Cookie.SameSite.valueOf(""));
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST");
 	}
 
 }
