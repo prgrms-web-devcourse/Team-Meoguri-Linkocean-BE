ac22f3d935f3b83ad09b4760f93886d8ea81c3cf
hyuk0309 <bahngeh@naver.com>
2022-08-05 01:31:05 +0900

    refactor : 사용하지 않는 컨트롤러 주석처리

--- src/main/java/com/meoguri/linkocean/controller/bookmark/TagController.java
+++ src/main/java/com/meoguri/linkocean/controller/bookmark/TagController.java
@@ -1,17 +1,8 @@
 package com.meoguri.linkocean.controller.bookmark;
 
-import static java.util.stream.Collectors.*;
-
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.meoguri.linkocean.configuration.security.oauth.LoginUser;
-import com.meoguri.linkocean.configuration.security.oauth.SessionUser;
-import com.meoguri.linkocean.controller.bookmark.dto.GetMyTagsResponse;
-import com.meoguri.linkocean.controller.common.PageResponse;
 import com.meoguri.linkocean.domain.bookmark.service.TagService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,13 +14,17 @@ public class TagController {
 
 	private final TagService tagService;
 
-	@GetMapping
-	public PageResponse<GetMyTagsResponse> getMyTags(@LoginUser SessionUser sessionUser) {
-
-		final List<GetMyTagsResponse> tags = tagService.getMyTags(sessionUser.getId()).stream()
-			.map(GetMyTagsResponse::ofResult)
-			.collect(toList());
-
-		return PageResponse.of("tags", tags);
-	}
+	/**
+	 * Tag Controller 는 없어도 될 것 같습니다.
+	 * 사용자 태그 목록 조회 API가 따로 필요할 것 같아 초벌로 만들어 두신 것 같지만, 사용자 태그 목록 조회 API가 내 프로필 조회 API에 합쳐지면서 필요없게 됐습니다.
+	 */
+	// @GetMapping
+	// public PageResponse<GetMyTagsResponse> getMyTags(@LoginUser SessionUser sessionUser) {
+	//
+	// 	final List<GetMyTagsResponse> tags = tagService.getMyTags(sessionUser.getId()).stream()
+	// 		.map(GetMyTagsResponse::ofResult)
+	// 		.collect(toList());
+	//
+	// 	return PageResponse.of("tags", tags);
+	// }
 }
