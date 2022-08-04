07635b3b7e77cfd359f71eb7e374d864273c5c37
ndy2 <emrdbs12@gmail.com>
2022-08-04 16:01:59 +0900

    fix: 복붙하다 잘못한거 수정

--- src/main/java/com/meoguri/linkocean/controller/common/SliceResponseJsonSerializer.java
+++ src/main/java/com/meoguri/linkocean/controller/common/SliceResponseJsonSerializer.java
@@ -10,10 +10,10 @@ import com.fasterxml.jackson.databind.SerializerProvider;
  * 목록 조회의 공통 Serializer
  * - 데이터 타입 별로 array 의 이름을 커스터마이징 하기 위해 사용
  */
-public class SliceResponseJsonSerializer extends JsonSerializer<PageResponse<?>> {
+public class SliceResponseJsonSerializer extends JsonSerializer<SliceResponse<?>> {
 
 	@Override
-	public void serialize(final PageResponse value, final JsonGenerator gen, final SerializerProvider serializers)
+	public void serialize(final SliceResponse value, final JsonGenerator gen, final SerializerProvider serializers)
 		throws IOException {
 
 		gen.writeStartObject();
