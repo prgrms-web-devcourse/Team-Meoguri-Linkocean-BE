56ea8c4476988f92fbb434c2063db8d5ba12375e
SeungHan <jk05018@naver.com>
2022-08-04 14:21:13 +0900

    refactor : session-jdbc 부활

--- src/main/java/com/meoguri/linkocean/configuration/WebConfiguration.java
+++ src/main/java/com/meoguri/linkocean/configuration/WebConfiguration.java
@@ -1,8 +1,12 @@
 package com.meoguri.linkocean.configuration;
 
+import java.net.MalformedURLException;
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -31,6 +35,14 @@ public class WebConfiguration implements WebMvcConfigurer {
 			.allowedOrigins("http://localhost:8080", "http://localhost:3000")
 			.allowedMethods("GET", "POST")
 			.allowCredentials(true); //쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
+	}
+
+	@Bean
+	public CookieSerializer cookieSerializer() throws MalformedURLException {
+		DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+		serializer.setCookieName("JSESSIONID");
+		serializer.setSameSite("");
+		return serializer;
 	}
 
 }
