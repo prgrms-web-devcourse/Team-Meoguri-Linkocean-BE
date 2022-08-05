0a158bb9eb149e6a014e8606b4d1d5bbc3f46be9
hyuk0309 <bahngeh@naver.com>
2022-08-05 10:56:49 +0900

    refactor : lombok 적용 가능한 곳에 lombok 적용하기

--- src/main/java/com/meoguri/linkocean/controller/common/PageResponse.java
+++ src/main/java/com/meoguri/linkocean/controller/common/PageResponse.java
@@ -6,20 +6,16 @@ import java.util.List;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 @JsonSerialize(using = PageResponseJsonSerializer.class)
 public class PageResponse<T> {
 
 	private final long totalCount;
 	private final String name;
 	private final List<T> data;
-
-	public PageResponse(final long totalCount, final String name, final List<T> data) {
-		this.totalCount = totalCount;
-		this.name = name;
-		this.data = data;
-	}
 
 	public static <T> PageResponse<T> of(final long totalCount, final String name, final List<T> data) {
 		return new PageResponse<>(totalCount, name, data);
--- src/main/java/com/meoguri/linkocean/domain/bookmark/service/dto/PageResult.java
+++ src/main/java/com/meoguri/linkocean/domain/bookmark/service/dto/PageResult.java
@@ -3,17 +3,14 @@ package com.meoguri.linkocean.domain.bookmark.service.dto;
 import java.util.List;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class PageResult<T> {
 
 	private final long totalCount;
 	private final List<T> data;
-
-	public PageResult(final long totalCount, final List<T> data) {
-		this.totalCount = totalCount;
-		this.data = data;
-	}
 
 	public static <T> PageResult<T> of(final long totalCount, final List<T> data) {
 		return new PageResult<>(totalCount, data);
