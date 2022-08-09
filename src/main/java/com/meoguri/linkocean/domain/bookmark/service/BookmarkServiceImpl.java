3701c807507555dc59749f032e3d8bc49cebc1a7
ndy2 <emrdbs12@gmail.com>
2022-08-10 04:41:38 +0900

    style: line-length-120

--- src/main/java/com/meoguri/linkocean/domain/bookmark/service/BookmarkServiceImpl.java
+++ src/main/java/com/meoguri/linkocean/domain/bookmark/service/BookmarkServiceImpl.java
@@ -189,7 +189,8 @@ public class BookmarkServiceImpl implements BookmarkService {
 
 		// 추가 정보 조회
 		final List<Boolean> isFavorites = checkIsFavoriteQuery.isFavorites(currentUserProfileId, bookmarks);
-		final List<Boolean> isWriters = Collections.emptyList(); // checkIsWriterQuery.checkIsWriter(currentUserProfileId, bookmarks);
+		//final List<Boolean> isWriters =  checkIsWriterQuery.checkIsWriter(currentUserProfileId, bookmarks);
+		final List<Boolean> isWriters = Collections.emptyList();
 		final List<Boolean> isFollows = checkIsFollowQuery.isFollows(currentUserProfileId, writers);
 
 		return toResultPage(bookmarkPage, isFavorites, isWriters, isFollows, pageable);
