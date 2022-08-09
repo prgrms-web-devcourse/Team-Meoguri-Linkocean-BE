893bc1f5305a3886b27c03d2ae11e8886f95af8d
hyuk0309 <bahngeh@naver.com>
2022-08-09 18:20:24 +0900

    feat : 궁극의 프로필 목록 조회 Repository API 개발

--- src/main/java/com/meoguri/linkocean/domain/profile/persistence/CustomProfileRepositoryImpl.java
+++ src/main/java/com/meoguri/linkocean/domain/profile/persistence/CustomProfileRepositoryImpl.java
@@ -8,18 +8,46 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.meoguri.linkocean.domain.profile.entity.Profile;
 import com.meoguri.linkocean.domain.profile.persistence.dto.ProfileFindCond;
+import com.meoguri.linkocean.domain.profile.persistence.dto.UltimateProfileFindCond;
 import com.meoguri.linkocean.util.Querydsl4RepositorySupport;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
 
 @Repository
 public class CustomProfileRepositoryImpl extends Querydsl4RepositorySupport implements CustomProfileRepository {
 
 	public CustomProfileRepositoryImpl(final EntityManager em) {
 		super(Profile.class);
+	}
+
+	@Override
+	public Page<Profile> ultimateFindProfiles(final UltimateProfileFindCond findCond, final Pageable pageable) {
+
+		final Long currentProfileId = findCond.getProfileId();
+		final boolean isFollower = findCond.isFollower();
+		final boolean isFollowee = findCond.isFollowee();
+		final String username = findCond.getUsername();
+
+		final JPAQuery<Profile> base = selectFrom(profile);
+
+		if (isFollower) {
+			return applyPaginationWithoutTotalPage(
+				pageable, base.where(followerOfUsername(currentProfileId, username)));
+		}
+
+		if (isFollowee) {
+			return applyPaginationWithoutTotalPage(
+				pageable, base.where(followeeOfUsername(currentProfileId, username)));
+		}
+
+		return applyPaginationWithoutTotalPage(
+			pageable, base.where(usernameContains(username)));
 	}
 
 	@Override
@@ -66,7 +94,7 @@ public class CustomProfileRepositoryImpl extends Querydsl4RepositorySupport impl
 					.on(follow.follower.id.eq(profile.id))
 			).where(
 				follow.followee.id.eq(profileId),
-				usernameLike(username)
+				usernameContains(username)
 			))
 		);
 	}
@@ -82,7 +110,7 @@ public class CustomProfileRepositoryImpl extends Querydsl4RepositorySupport impl
 					.on(follow.followee.id.eq(profile.id))
 			).where(
 				follow.follower.id.eq(profileId),
-				usernameLike(username)
+				usernameContains(username)
 			))
 		);
 	}
@@ -91,5 +119,9 @@ public class CustomProfileRepositoryImpl extends Querydsl4RepositorySupport impl
 	private BooleanBuilder usernameLike(final String username) {
 
 		return nullSafeBuilder(() -> profile.username.like(String.join(username, "%", "%")));
+	}
+
+	private BooleanBuilder usernameContains(final String username) {
+		return nullSafeBuilder(() -> profile.username.containsIgnoreCase(username));
 	}
 }
