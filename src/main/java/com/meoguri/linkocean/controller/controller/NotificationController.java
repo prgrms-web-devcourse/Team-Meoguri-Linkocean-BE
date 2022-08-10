cf27080292ef85416b17a1faf10d498e8756f82d
ndy2 <emrdbs12@gmail.com>
2022-08-10 15:42:05 +0900

    feat: 알림 더미데이터 말기

---
+++ src/main/java/com/meoguri/linkocean/controller/controller/NotificationController.java
@@ -0,0 +1,82 @@
+package com.meoguri.linkocean.controller.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.meoguri.linkocean.configuration.security.jwt.SecurityUser;
+import com.meoguri.linkocean.controller.common.SliceResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/notifications")
+@RestController
+public class NotificationController {
+
+	// private final NotificationService notificationService;
+
+	/* 공유 알림 생성 */
+	@PostMapping
+	public void shareNotification(
+		final @AuthenticationPrincipal SecurityUser user,
+		final @RequestBody Map<String, Long> request
+	) {
+		request.get("targetId");
+		request.get("bookmarkId");
+	}
+
+	/* 알림 조회 */
+	@GetMapping
+	public SliceResponse<Map<String, Object>> getNotifications(
+		final @AuthenticationPrincipal SecurityUser user
+	) {
+		return SliceResponse.of("notifications", dummyNotifications());
+	}
+
+	private List<Map<String, Object>> dummyNotifications() {
+		final Map<String, Object> share = Map.of(
+			"type", "SHARE",
+			"info", Map.of(
+				"bookmark", Map.of(
+					"id", 1,
+					"title", "네이버",
+					"link", "https://www.naver.com"),
+				"sender", Map.of(
+					"id", 1,
+					"username", "haha"
+				)
+			)
+		);
+		final Map<String, Object> feed = Map.of(
+			"type", "FEED",
+			"info", Map.of(
+				"bookmark", Map.of(
+					"id", 2,
+					"title", "구글",
+					"link", "https://www.google.com"),
+				"sender", Map.of(
+					"id", 2,
+					"username", "jacob"
+				)
+			)
+		);
+		final Map<String, Object> old = Map.of(
+			"type", "OLD",
+			"info", Map.of(
+				"bookmark", Map.of(
+					"id", 3,
+					"title", "프로그래머스",
+					"link", "https://school.programmers.co.kr/my-courses/learning")
+			)
+		);
+
+		return List.of(share, feed, old);
+	}
+}
