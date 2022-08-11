9b45202962f537fdc5fb93925da649688b09c960
ndy2 <emrdbs12@gmail.com>
2022-08-12 03:41:01 +0900

    feat: NotificationController

--- src/main/java/com/meoguri/linkocean/controller/notification/NotificationController.java
+++ src/main/java/com/meoguri/linkocean/controller/notification/NotificationController.java
@@ -1,8 +1,9 @@
 package com.meoguri.linkocean.controller.notification;
 
-import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.meoguri.linkocean.configuration.security.jwt.SecurityUser;
 import com.meoguri.linkocean.controller.common.SliceResponse;
+import com.meoguri.linkocean.domain.notification.entity.Notification;
+import com.meoguri.linkocean.domain.notification.service.NotificationService;
+import com.meoguri.linkocean.domain.notification.service.dto.ShareNotificationCommand;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,63 +24,30 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class NotificationController {
 
-	// private final NotificationService notificationService;
+	private final NotificationService notificationService;
 
 	/* 공유 알림 생성 */
-	@PostMapping
+	@PostMapping("/share")
 	public void shareNotification(
 		final @AuthenticationPrincipal SecurityUser user,
 		final @RequestBody Map<String, Long> request
 	) {
-		request.get("targetId");
-		request.get("bookmarkId");
+		notificationService.shareNotification(
+			new ShareNotificationCommand(
+				user.getProfileId(),
+				request.get("targetId"),
+				request.get("bookmarkId")
+			)
+		);
 	}
 
 	/* 알림 조회 */
 	@GetMapping
-	public SliceResponse<Map<String, Object>> getNotifications(
-		final @AuthenticationPrincipal SecurityUser user
+	public SliceResponse<Notification> getNotifications(
+		final Pageable pageable
 	) {
-		return SliceResponse.of("notifications", dummyNotifications());
-	}
+		final Slice<Notification> result = notificationService.getNotifications(pageable);
 
-	private List<Map<String, Object>> dummyNotifications() {
-		final Map<String, Object> share = Map.of(
-			"type", "SHARE",
-			"info", Map.of(
-				"bookmark", Map.of(
-					"id", 1,
-					"title", "네이버",
-					"link", "https://www.naver.com"),
-				"sender", Map.of(
-					"id", 1,
-					"username", "haha"
-				)
-			)
-		);
-		final Map<String, Object> feed = Map.of(
-			"type", "FEED",
-			"info", Map.of(
-				"bookmark", Map.of(
-					"id", 2,
-					"title", "구글",
-					"link", "https://www.google.com"),
-				"sender", Map.of(
-					"id", 2,
-					"username", "jacob"
-				)
-			)
-		);
-		final Map<String, Object> old = Map.of(
-			"type", "OLD",
-			"info", Map.of(
-				"bookmark", Map.of(
-					"id", 3,
-					"title", "프로그래머스",
-					"link", "https://school.programmers.co.kr/my-courses/learning")
-			)
-		);
-
-		return List.of(share, feed, old);
+		return SliceResponse.of("notifications", result.getContent());
 	}
 }
