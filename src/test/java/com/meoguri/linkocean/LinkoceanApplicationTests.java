69fd77ec57138b4df09a93fee9a8ac56a6045d20
hyuk0309 <bahngeh@naver.com>
2022-08-13 00:47:27 +0900

    feat : local 프로파일 부활

--- src/test/java/com/meoguri/linkocean/LinkoceanApplicationTests.java
+++ src/test/java/com/meoguri/linkocean/LinkoceanApplicationTests.java
@@ -3,9 +3,11 @@ package com.meoguri.linkocean;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.test.context.ActiveProfiles;
 
 @EnableAspectJAutoProxy
 @SpringBootTest
+@ActiveProfiles("local")
 class LinkoceanApplicationTests {
 
 	@Test
