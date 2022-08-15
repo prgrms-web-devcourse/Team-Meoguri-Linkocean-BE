0da141f88811a5fdca40b73b4747969b06bdd657
SeungHan <jk05018@naver.com>
2022-08-15 17:47:02 +0900

    feat: Bookmark RestDocs 완료

--- src/test/java/com/meoguri/linkocean/controller/restdocs/BookmarkDocsController.java
+++ src/test/java/com/meoguri/linkocean/controller/restdocs/BookmarkDocsController.java
@@ -7,7 +7,6 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.List;
@@ -77,7 +76,7 @@ public class BookmarkDocsController extends RestDocsTestSupport {
 	}
 
 	@Test
-	void 북마크_수정_Api_성공() throws Exception {
+	void 북마크_수정_api() throws Exception {
 		//given
 		final long bookmarkId = 북마크_등록(링크_메타데이터_얻기("https://www.naver.com"), "IT", List.of("good", "spring"), "all");
 
@@ -87,10 +86,11 @@ public class BookmarkDocsController extends RestDocsTestSupport {
 		final String updateOpenType = "all";
 		final List<String> updateTags = List.of("Spring", "React", "LinkOcean");
 
-		final UpdateBookmarkRequest updateBookmarkRequest = new UpdateBookmarkRequest(updateTitle, updateMemo,
-			updateCategory, updateOpenType, updateTags);
+		final UpdateBookmarkRequest updateBookmarkRequest = new UpdateBookmarkRequest(
+			updateTitle, updateMemo, updateCategory, updateOpenType, updateTags);
+
 		//when
-		mockMvc.perform(put(basePath + "/{bookmarkId}", bookmarkId)
+		mockMvc.perform(RestDocumentationRequestBuilders.put(basePath + "/{bookmarkId}", bookmarkId)
 				.header(AUTHORIZATION, token)
 				.contentType(APPLICATION_JSON)
 				.content(createJson(updateBookmarkRequest)))
@@ -146,157 +146,170 @@ public class BookmarkDocsController extends RestDocsTestSupport {
 
 	}
 
-	@Nested
-	class 내_북마크_목록_조회_테스트 {
+	@Test
+	void 내_북마크_목록_조회_api() throws Exception {
+		long bookmarkId1 = 북마크_등록(링크_메타데이터_얻기("https://www.naver.com"), "title1", "IT", List.of("공부"), "all");
+		long bookmarkId2 = 북마크_등록(링크_메타데이터_얻기("https://www.airbnb.co.kr"), "title2", "여행", List.of("travel"),
+			"partial");
 
-		private long bookmarkId1;
-		private long bookmarkId2;
+		//when
+		mockMvc.perform(get(basePath + "/me")
+				.header(AUTHORIZATION, token)
+				.accept(APPLICATION_JSON))
+			//then
+			.andExpect(status().isOk())
+			.andExpectAll(
+				jsonPath("$.totalCount").value(2),
+				jsonPath("$.bookmarks", hasSize(2)),
+				jsonPath("$.bookmarks[0].id").value(bookmarkId2),        // 입력 시간 역순으로 조회
+				jsonPath("$.bookmarks[1].id").value(bookmarkId1),
+				jsonPath("$.bookmarks[1].title").value("title1"),
+				jsonPath("$.bookmarks[1].url").value("https://www.naver.com"),
+				jsonPath("$.bookmarks[1].openType").value("all"),
+				jsonPath("$.bookmarks[1].updatedAt").exists(),
+				jsonPath("$.bookmarks[1].imageUrl").exists(),
+				jsonPath("$.bookmarks[1].likeCount").value(0),
+				jsonPath("$.bookmarks[1].isFavorite").value(false),
+				jsonPath("$.bookmarks[1].isWriter").value(true),
+				jsonPath("$.bookmarks[1].tags", hasItem("공부"))
+			)
 
-		@BeforeEach
-		void setUp() throws Exception {
-			bookmarkId1 = 북마크_등록(링크_메타데이터_얻기("https://www.naver.com"), "title1", "IT", List.of("공부"), "all");
-			bookmarkId2 = 북마크_등록(링크_메타데이터_얻기("https://www.airbnb.co.kr"), "title2", "여행", List.of("travel"), "partial");
-		}
-
-		@Test
-		void 내_북마크_목록_조회_Api_성공_필터링_조건_없이_조회() throws Exception {
-			//when
-			mockMvc.perform(get(basePath + "/me")
-					.header(AUTHORIZATION, token)
-					.accept(APPLICATION_JSON))
-				//then
-				.andExpect(status().isOk())
-				.andExpectAll(
-					jsonPath("$.totalCount").value(2),
-					jsonPath("$.bookmarks", hasSize(2)),
-					jsonPath("$.bookmarks[0].id").value(bookmarkId2),        // 입력 시간 역순으로 조회
-					jsonPath("$.bookmarks[1].id").value(bookmarkId1),
-					jsonPath("$.bookmarks[1].title").value("title1"),
-					jsonPath("$.bookmarks[1].url").value("https://www.naver.com"),
-					jsonPath("$.bookmarks[1].openType").value("all"),
-					jsonPath("$.bookmarks[1].updatedAt").exists(),
-					jsonPath("$.bookmarks[1].imageUrl").exists(),
-					jsonPath("$.bookmarks[1].likeCount").value(0),
-					jsonPath("$.bookmarks[1].isFavorite").value(false),
-					jsonPath("$.bookmarks[1].isWriter").value(true),
-					jsonPath("$.bookmarks[1].tags", hasItem("공부"))
-				).andDo(print());
-		}
-
-		@Test
-		void 내_북마크_목록_조회_Api_성공_카테고리_필터링() throws Exception {
-			//when
-			mockMvc.perform(get(basePath + "/me")
-					.param("category", "IT")
-					.header(AUTHORIZATION, token)
-					.accept(APPLICATION_JSON))
-				//then
-				.andExpect(status().isOk())
-				.andExpectAll(
-					jsonPath("$.totalCount").value(1),
-					jsonPath("$.bookmarks[0].category").value("IT")
+			//docs
+			.andDo(
+				restDocs.document(
+					requestHeaders(
+						headerWithName(AUTHORIZATION).description("인증 토큰")
+					),
+					requestParameters(
+						parameterWithName("page").optional().description("현재 페이지(page)"),
+						parameterWithName("size").optional().description("북마크 개수(size)"),
+						parameterWithName("order").optional().description("북마크 정렬 기준"),
+						parameterWithName("tags").optional().description("필터링 태그 목록"),
+						parameterWithName("category").optional().description("카테고리"),
+						parameterWithName("searchTitle").optional().description(""),
+						parameterWithName("favorite").optional().description("필터링 태그 목록")
+					),
+					responseFields(
+						fieldWithPath("totalCount").description("북마크 총 개수"),
+						fieldWithPath("bookmarks[]").optional().description("북마크 목록"),
+						fieldWithPath("bookmarks[].id").description("북마크 ID"),
+						fieldWithPath("bookmarks[].title").description("북마크 제목"),
+						fieldWithPath("bookmarks[].url").description("북마크 url"),
+						fieldWithPath("bookmarks[].openType").description("북마크 공개 범위"),
+						fieldWithPath("bookmarks[].category").description("북마크 카테고리"),
+						fieldWithPath("bookmarks[].updatedAt").description("북마크 수정 일자"),
+						fieldWithPath("bookmarks[].imageUrl").description("북마크 이미지 url"),
+						fieldWithPath("bookmarks[].likeCount").description("북마크 좋아요 수"),
+						fieldWithPath("bookmarks[].isFavorite").description("북마크 즐겨찾기 여부"),
+						fieldWithPath("bookmarks[].isWriter").description("북마크 작성자 여부"),
+						fieldWithPath("bookmarks[].tags[]").optional().description("북마크 태그 목록")
+					)
 				)
-				.andDo(print());
-		}
-
-		@Test
-		void 내_북마크_목록_조회_Api_성공_즐겨찾기_필터링() throws Exception {
-			//given
-			북마크_즐겨찾기(bookmarkId1);
-
-			//when
-			mockMvc.perform(get(basePath + "/me")
-					.param("favorite", "true")
-					.header(AUTHORIZATION, token)
-					.accept(APPLICATION_JSON))
-				//then
-				.andExpect(status().isOk())
-				.andExpectAll(
-					jsonPath("$.totalCount").value(1),
-					jsonPath("$.bookmarks[0].id").value(bookmarkId1),
-					jsonPath("$.bookmarks[0].isFavorite").value(true)
-				).andDo(print());
-		}
-
-		@Test
-		void 내_북마크_목록_조회_Api_성공_태그_필터링() throws Exception {
-			//when
-			mockMvc.perform(get(basePath + "/me")
-					.header(AUTHORIZATION, token)
-					.param("tags", "공부,travel")
-					.accept(APPLICATION_JSON))
-				//then
-				.andExpect(status().isOk())
-				.andExpectAll(
-					jsonPath("$.totalCount").value(2)
-				).andDo(print());
-		}
+			);
 	}
 
 	@Test
-	void url_중복확인_url() throws Exception {
+	void url_중복확인_api() throws Exception {
 		//given
 		final long bookmarkId = 북마크_등록(링크_메타데이터_얻기("https://www.google.com"), "title1", "IT", List.of("공부"), "all");
 		final String expectedLocationHeader = "api/v1/bookmarks/" + bookmarkId;
 
 		//when
-		mockMvc.perform(get(basePath + "?url=https://www.google.com")
+		mockMvc.perform(RestDocumentationRequestBuilders.get(basePath + "?url=https://www.google.com")
 				.header(AUTHORIZATION, token)
 				.accept(APPLICATION_JSON))
 			//then
 			.andExpect(status().isOk())
 			.andExpect(header().string("Location", expectedLocationHeader))
 			.andExpect(jsonPath("$.isDuplicateUrl").value(true))
-			.andDo(print());
+
+			//docs
+			.andDo(
+				restDocs.document(
+					requestHeaders(
+						headerWithName(AUTHORIZATION).description("인증 토큰")
+					),
+					requestParameters(
+						parameterWithName("url").description("북마크 url")
+					),
+					responseHeaders(
+						headerWithName("Location").optional().description("중복된 북마크 location")
+					),
+					responseFields(
+						fieldWithPath("isDuplicateUrl").optional().description("중복 여부")
+					)
+				)
+			);
 	}
 
-	@Nested
-	class 다른_사람_북마크_목록_조회_api {
+	@Test
+	void 다른_사람_북마크_목록_조회_api() throws Exception {
+		//given
+		유저_등록_로그인("crush@gmail.com", "GOOGLE");
+		프로필_등록("crush", List.of("IT"));
 
-		private long bookmarkId1;
-		private long bookmarkId2;
-		private long bookmarkId4;
+		유저_등록_로그인("otherUser@gmail.com", "GOOGLE");
+		long otherProfileId = 프로필_등록("user1", List.of("IT"));
 
-		private long otherProfileId;
+		long bookmarkId1 = 북마크_등록(링크_메타데이터_얻기("https://www.naver.com"), "title1", "IT", List.of("공부"), "all");
+		long bookmarkId2 = 북마크_등록(링크_메타데이터_얻기("https://www.airbnb.co.kr"), "title2", "여행", List.of("travel"),
+			"partial");
+		북마크_등록(링크_메타데이터_얻기("https://programmers.co.kr"), "title3", "기술", List.of("공부", "코테"), "private");
+		long bookmarkId4 = 북마크_등록(링크_메타데이터_얻기("https://www.google.com"), "title4", "자기계발", List.of("머구리"), "all");
 
-		@BeforeEach
-		void setUp() throws Exception {
+		로그인("crush@gmail.com", "GOOGLE");
 
-			유저_등록_로그인("crush@gmail.com", "GOOGLE");
-			프로필_등록("crush", List.of("IT"));
+		팔로우(otherProfileId);
 
-			유저_등록_로그인("otherUser@gmail.com", "GOOGLE");
-			otherProfileId = 프로필_등록("user1", List.of("IT"));
+		//when then
+		mockMvc.perform(RestDocumentationRequestBuilders.get(basePath + "/others/{profileId}", otherProfileId)
+				.header(AUTHORIZATION, token)
+				.accept(APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpectAll(
+				jsonPath("$.totalCount").value(3),
+				jsonPath("$.bookmarks[0].id").value(bookmarkId4),
+				jsonPath("$.bookmarks[0].openType").value("all"),
+				jsonPath("$.bookmarks[1].id").value(bookmarkId2),
+				jsonPath("$.bookmarks[1].openType").value("partial"),
+				jsonPath("$.bookmarks[2].id").value(bookmarkId1),
+				jsonPath("$.bookmarks[2].openType").value("all"))
 
-			bookmarkId1 = 북마크_등록(링크_메타데이터_얻기("https://www.naver.com"), "title1", "IT", List.of("공부"), "all");
-			bookmarkId2 = 북마크_등록(링크_메타데이터_얻기("https://www.airbnb.co.kr"), "title2", "여행", List.of("travel"), "partial");
-			북마크_등록(링크_메타데이터_얻기("https://programmers.co.kr"), "title3", "기술", List.of("공부", "코테"), "private");
-			bookmarkId4 = 북마크_등록(링크_메타데이터_얻기("https://www.google.com"), "title4", "자기계발", List.of("머구리"), "all");
-
-			로그인("crush@gmail.com", "GOOGLE");
-		}
-
-		@Test
-		void 팔로우_유저_북마크_목록_조회() throws Exception {
-			//given
-			팔로우(otherProfileId);
-
-			//when then
-			mockMvc.perform(get(basePath + "/others/{profileId}", otherProfileId)
-					.header(AUTHORIZATION, token)
-					.accept(APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpectAll(
-					jsonPath("$.totalCount").value(3),
-					jsonPath("$.bookmarks[0].id").value(bookmarkId4),
-					jsonPath("$.bookmarks[0].openType").value("all"),
-					jsonPath("$.bookmarks[1].id").value(bookmarkId2),
-					jsonPath("$.bookmarks[1].openType").value("partial"),
-					jsonPath("$.bookmarks[2].id").value(bookmarkId1),
-					jsonPath("$.bookmarks[2].openType").value("all"))
-				.andDo(print());
-		}
-
+			//docs
+			.andDo(
+				restDocs.document(
+					requestHeaders(
+						headerWithName(AUTHORIZATION).description("인증 토큰")
+					),
+					pathParameters(
+						parameterWithName("profileId").description("다른 사용자 프로필 ID")
+					),
+					requestParameters(
+						parameterWithName("page").optional().description("현재 페이지(page)"),
+						parameterWithName("size").optional().description("북마크 개수(size)"),
+						parameterWithName("order").optional().description("북마크 정렬 기준"),
+						parameterWithName("tags").optional().description("필터링 태그 목록"),
+						parameterWithName("category").optional().description("카테고리"),
+						parameterWithName("searchTitle").optional().description("제목 검색"),
+						parameterWithName("favorite").optional().description("필터링 태그 목록")
+					),
+					responseFields(
+						fieldWithPath("totalCount").description("북마크 총 개수"),
+						fieldWithPath("bookmarks[]").optional().description("북마크 목록"),
+						fieldWithPath("bookmarks[].id").description("북마크 ID"),
+						fieldWithPath("bookmarks[].title").description("북마크 제목"),
+						fieldWithPath("bookmarks[].url").description("북마크 url"),
+						fieldWithPath("bookmarks[].openType").description("북마크 공개 범위"),
+						fieldWithPath("bookmarks[].category").description("북마크 카테고리"),
+						fieldWithPath("bookmarks[].updatedAt").description("북마크 수정 일자"),
+						fieldWithPath("bookmarks[].imageUrl").description("북마크 이미지 url"),
+						fieldWithPath("bookmarks[].likeCount").description("북마크 좋아요 수"),
+						fieldWithPath("bookmarks[].isFavorite").description("북마크 즐겨찾기 여부"),
+						fieldWithPath("bookmarks[].isWriter").description("북마크 작성자 여부"),
+						fieldWithPath("bookmarks[].tags[]").optional().description("북마크 태그 목록")
+					)
+				)
+			);
 	}
 
 	@Nested
@@ -338,7 +351,7 @@ public class BookmarkDocsController extends RestDocsTestSupport {
 		}
 
 		@Test
-		void 피드_북마크_조회_성공() throws Exception {
+		void 피드_북마크_조회_api() throws Exception {
 			//when
 			mockMvc.perform(get(basePath + "/feed")
 					.header(AUTHORIZATION, token)
@@ -354,43 +367,45 @@ public class BookmarkDocsController extends RestDocsTestSupport {
 					jsonPath("$.bookmarks[3].id").value(bookmarkId7),
 					jsonPath("$.bookmarks[4].id").value(bookmarkId8),
 					jsonPath("$.bookmarks[5].id").value(bookmarkId10)
-				).andDo(print());
+				)
+
+				//docs
+				.andDo(
+					restDocs.document(
+						requestHeaders(
+							headerWithName(AUTHORIZATION).description("인증 토큰")
+						),
+						requestParameters(
+							parameterWithName("page").optional().description("현재 페이지(page)"),
+							parameterWithName("size").optional().description("북마크 개수(size)"),
+							parameterWithName("order").optional().description("북마크 정렬 기준"),
+							parameterWithName("category").optional().description("카테고리"),
+							parameterWithName("searchTitle").optional().description("제목 검색"),
+							parameterWithName("follow").optional().description("팔로우 여부")
+						),
+						responseFields(
+							fieldWithPath("totalCount").description("북마크 총 개수"),
+							fieldWithPath("bookmarks[]").optional().description("북마크 목록"),
+							fieldWithPath("bookmarks[].id").description("북마크 ID"),
+							fieldWithPath("bookmarks[].title").description("북마크 제목"),
+							fieldWithPath("bookmarks[].url").description("북마크 url"),
+							fieldWithPath("bookmarks[].openType").description("북마크 공개 범위"),
+							fieldWithPath("bookmarks[].category").description("북마크 카테고리"),
+							fieldWithPath("bookmarks[].updatedAt").description("북마크 수정 일자"),
+							fieldWithPath("bookmarks[].imageUrl").description("북마크 이미지 url"),
+							fieldWithPath("bookmarks[].likeCount").description("북마크 좋아요 수"),
+							fieldWithPath("bookmarks[].isFavorite").description("북마크 즐겨찾기 여부"),
+							fieldWithPath("bookmarks[].isWriter").description("북마크 작성자 여부"),
+							fieldWithPath("bookmarks[].tags[]").optional().description("북마크 태그 목록"),
+							fieldWithPath("bookmarks[].profile").description("작성자"),
+							fieldWithPath("bookmarks[].profile.profileId").description("작성자 ID"),
+							fieldWithPath("bookmarks[].profile.imageUrl").description("작성자 이미지 url"),
+							fieldWithPath("bookmarks[].profile.username").description("작성자 이름"),
+							fieldWithPath("bookmarks[].profile.isFollow").description("작성자 팔로우 여부")
+						)
+					)
+				);
 		}
 
-		@Test
-		void 피드_북마크_조회_팔로우_여부로_성공() throws Exception {
-			//when
-			mockMvc.perform(get(basePath + "/feed")
-					.param("follow", "true")
-					.header(AUTHORIZATION, token)
-					.accept(APPLICATION_JSON))
-				//then
-				.andExpect(status().isOk())
-				.andExpectAll(
-					jsonPath("$.totalCount").value(2),
-					jsonPath("$.bookmarks", hasSize(2)),
-					jsonPath("$.bookmarks[0].id").value(bookmarkId7),
-					jsonPath("$.bookmarks[1].id").value(bookmarkId8)
-				).andDo(print());
-		}
-
-		@Test
-		void 피드_북마크_조회_즐겨찾기_후_조회_성공() throws Exception {
-			로그인("user2@gmail.com", "GOOGLE");
-			북마크_즐겨찾기(bookmarkId10);
-
-			//when
-			mockMvc.perform(get(basePath + "/feed")
-					.param("favorite", "true")
-					.header(AUTHORIZATION, token)
-					.accept(APPLICATION_JSON))
-				//then
-				.andExpect(status().isOk())
-				.andExpectAll(
-					jsonPath("$.totalCount").value(1),
-					jsonPath("$.bookmarks", hasSize(1)),
-					jsonPath("$.bookmarks[0].isWriter").value(false)
-				).andDo(print());
-		}
 	}
 }
