e5889ad558d1f4b320e5a383a4124894108ffba8
ndy2 <emrdbs12@gmail.com>
2022-08-18 15:29:30 +0900

    test: introduce BasePersistenceTest

--- src/test/java/com/meoguri/linkocean/support/persistence/BasePersistenceTest.java
+++ src/test/java/com/meoguri/linkocean/support/persistence/BasePersistenceTest.java
@@ -1,10 +1,22 @@
 package com.meoguri.linkocean.support.persistence;
 
+import static com.meoguri.linkocean.support.common.Fixture.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.meoguri.linkocean.domain.user.entity.vo.OAuthType;
+import com.meoguri.linkocean.domain.user.persistence.UserRepository;
 
 @Transactional
 @DataJpaTest
 public class BasePersistenceTest {
 
+	@Autowired
+	private UserRepository userRepository;
+
+	protected void 사용자_저장(final String email, final OAuthType oAuthType) {
+		userRepository.save(createUser(email, oAuthType));
+	}
 }
