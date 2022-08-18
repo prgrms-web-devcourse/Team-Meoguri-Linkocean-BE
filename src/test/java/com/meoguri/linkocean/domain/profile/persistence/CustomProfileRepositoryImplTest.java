f20a82e290189e696d7f646069337cff319b78b4
ndy2 <emrdbs12@gmail.com>
2022-08-18 16:28:41 +0900

    test: apply BasePersistenceTest on CustomProfileRepositoryImplTest

--- src/test/java/com/meoguri/linkocean/domain/profile/persistence/CustomProfileRepositoryImplTest.java
+++ src/test/java/com/meoguri/linkocean/domain/profile/persistence/CustomProfileRepositoryImplTest.java
@@ -1,43 +1,28 @@
 package com.meoguri.linkocean.domain.profile.persistence;
 
+import static com.meoguri.linkocean.domain.bookmark.entity.vo.Category.*;
 import static com.meoguri.linkocean.domain.user.entity.vo.OAuthType.*;
 import static com.meoguri.linkocean.support.common.Fixture.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 
-import com.meoguri.linkocean.domain.profile.entity.Follow;
 import com.meoguri.linkocean.domain.profile.entity.Profile;
 import com.meoguri.linkocean.domain.profile.persistence.dto.ProfileFindCond;
-import com.meoguri.linkocean.domain.user.entity.User;
-import com.meoguri.linkocean.domain.user.persistence.UserRepository;
 import com.meoguri.linkocean.support.common.CustomP6spySqlFormat;
+import com.meoguri.linkocean.support.persistence.BasePersistenceTest;
 
 @Import(CustomP6spySqlFormat.class)
-@DataJpaTest
-class CustomProfileRepositoryImplTest {
+class CustomProfileRepositoryImplTest extends BasePersistenceTest {
 
 	@Autowired
 	private ProfileRepository profileRepository;
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private FollowRepository followRepository;
-
-	@PersistenceContext
-	private EntityManager em;
 
 	private Profile profile1;
 	private Profile profile2;
@@ -46,21 +31,17 @@ class CustomProfileRepositoryImplTest {
 	@BeforeEach
 	void setUp() {
 		//set up 3 users
-		User user1 = userRepository.save(createUser("user1@gmail.com", GOOGLE));
-		User user2 = userRepository.save(createUser("user2@naver.com", NAVER));
-		User user3 = userRepository.save(createUser("user3@kakao.com", KAKAO));
-
-		profile1 = profileRepository.save(new Profile(user1, "user1"));
-		profile2 = profileRepository.save(new Profile(user2, "user2"));
-		profile3 = profileRepository.save(new Profile(user3, "user3"));
+		profile1 = 사용자_프로필_저장_등록("user1@gmail.com", GOOGLE, "user1", IT);
+		profile2 = 사용자_프로필_저장_등록("user2@naver.com", NAVER, "user2", IT);
+		profile3 = 사용자_프로필_저장_등록("user3@kakao.com", KAKAO, "user3", IT);
 	}
 
 	@Test
 	void 팔로워_목록_조회_성공_이름_지정_X() {
 		//given
-		followRepository.save(new Follow(profile1, profile2));
-		followRepository.save(new Follow(profile1, profile3));
-		followRepository.save(new Follow(profile2, profile3));
+		팔로우_저장(profile1, profile2);
+		팔로우_저장(profile1, profile3);
+		팔로우_저장(profile2, profile3);
 
 		//when
 		final Slice<Profile> followerOfUser1 = profileRepository.findProfiles(
@@ -79,8 +60,8 @@ class CustomProfileRepositoryImplTest {
 	@Test
 	void 팔로워_목록_조회_성공_이름_지정() {
 		//given
-		followRepository.save(new Follow(profile1, profile3));
-		followRepository.save(new Follow(profile2, profile3));
+		팔로우_저장(profile1, profile3);
+		팔로우_저장(profile2, profile3);
 
 		//when
 		final Slice<Profile> followerOfUser1 = profileRepository.findProfiles(
@@ -93,29 +74,29 @@ class CustomProfileRepositoryImplTest {
 	@Test
 	void 팔로이_목록_조회_성공_이름_지정_X() {
 		//given
-		followRepository.save(new Follow(profile1, profile2));
-		followRepository.save(new Follow(profile1, profile3));
-		followRepository.save(new Follow(profile2, profile3));
+		팔로우_저장(profile1, profile2);
+		팔로우_저장(profile1, profile3);
+		팔로우_저장(profile2, profile3);
 
 		//when
-		final Slice<Profile> followerOfUser1 = profileRepository.findProfiles(
+		final Slice<Profile> followeeOfUser1 = profileRepository.findProfiles(
 			condWhenFindFollowees(profile1.getId()), defaultPageable());
-		final Slice<Profile> followerOfUser2 = profileRepository.findProfiles(
+		final Slice<Profile> followeeOfUser2 = profileRepository.findProfiles(
 			condWhenFindFollowees(profile2.getId()), defaultPageable());
-		final Slice<Profile> followerOfUser3 = profileRepository.findProfiles(
+		final Slice<Profile> followeeOfUser3 = profileRepository.findProfiles(
 			condWhenFindFollowees(profile3.getId()), defaultPageable());
 
 		//then
-		assertThat(followerOfUser1).containsExactly(profile2, profile3);
-		assertThat(followerOfUser2).containsExactly(profile3);
-		assertThat(followerOfUser3).isEmpty();
+		assertThat(followeeOfUser1).containsExactly(profile2, profile3);
+		assertThat(followeeOfUser2).containsExactly(profile3);
+		assertThat(followeeOfUser3).isEmpty();
 	}
 
 	@Test
 	void 팔로이_목록_조회_성공_이름_지정() {
 		//given
-		followRepository.save(new Follow(profile1, profile2));
-		followRepository.save(new Follow(profile1, profile3));
+		팔로우_저장(profile1, profile2);
+		팔로우_저장(profile1, profile3);
 
 		//when
 		final Slice<Profile> followerOfUser1 = profileRepository.findProfiles(
