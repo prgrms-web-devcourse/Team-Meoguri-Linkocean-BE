fa2332b7ac5d7f9008995119d842c94a578468a0
ndy2 <emrdbs12@gmail.com>
2022-08-23 00:21:14 +0900

    refactor: BookmarkTag test 모두 통과

--- src/test/java/com/meoguri/linkocean/domain/bookmark/entity/TagsTest.java
+++ src/test/java/com/meoguri/linkocean/domain/bookmark/entity/TagsTest.java
@@ -1,32 +1,25 @@
 package com.meoguri.linkocean.domain.bookmark.entity;
 
+import static com.meoguri.linkocean.domain.bookmark.entity.Tags.*;
 import static com.meoguri.linkocean.test.support.common.Assertions.*;
-import static com.meoguri.linkocean.test.support.common.Fixture.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-
-import com.meoguri.linkocean.domain.bookmark.entity.vo.Category;
-import com.meoguri.linkocean.domain.bookmark.entity.vo.OpenType;
 
 class TagsTest {
 
 	@Test
 	void 북마크_업데이트_실패_태그_개수_초과() {
 		//given
-		final Tags tooManyTags = new Tags(List.of(
-			new Tag("tag1"),
-			new Tag("tag2"),
-			new Tag("tag3"),
-			new Tag("tag4"),
-			new Tag("tag5"),
-			new Tag("tag6")
-		));
+		final List<Tag> tooManyTags = new ArrayList<>();
+		for (int i = 0; i < MAX_TAGS_COUNT + 1; i++) {
+			tooManyTags.add(new Tag("tag" + i));
+		}
 
 		//when then
 		assertThatLinkoceanRuntimeException()
-			.isThrownBy(() -> createBookmark()
-				.update("updatedTitle", "updatedMemo", Category.HUMANITIES, OpenType.PRIVATE, tooManyTags));
+			.isThrownBy(() -> new Tags(tooManyTags));
 	}
 }
