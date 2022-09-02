8bcd2ad04ec5335c962b109bf397b367afaf6424
hyuk0309 <bahngeh@naver.com>
2022-09-02 16:19:16 +0900

    refactor(Default) : 상수 추가

--- src/main/java/com/meoguri/linkocean/controller/common/Default.java
+++ src/main/java/com/meoguri/linkocean/controller/common/Default.java
@@ -4,7 +4,8 @@ package com.meoguri.linkocean.controller.common;
  * 컨트롤러 계층에서 사용하는 기본 상수값들 모음
  */
 public enum Default {
-	IMAGE("default-image.png");
+	IMAGE("default-image.png"),
+	TITLE("제목 없음");
 
 	private final String text;
 
