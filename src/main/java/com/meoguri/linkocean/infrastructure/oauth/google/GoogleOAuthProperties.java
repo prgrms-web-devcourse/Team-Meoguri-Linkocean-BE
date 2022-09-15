8ca69afd791c58124f5e1d80c31e526686d52544
hyuk0309 <bahngeh@naver.com>
2022-09-15 20:05:11 +0900

    feat : 구글 소셜 로그인에 필요한 설정값 전용 객체 생성

---
+++ src/main/java/com/meoguri/linkocean/infrastructure/oauth/google/GoogleOAuthProperties.java
@@ -0,0 +1,21 @@
+package com.meoguri.linkocean.infrastructure.oauth.google;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "oauth.google")
+public class GoogleOAuthProperties {
+
+	private String url;
+	private String clientId;
+	private String clientSecret;
+	private String scope;
+	private String grantType;
+	private String redirectUrl;
+}
