1c85bf529c011da0a9dfa8e6cf798c6a2ca1fb7d
hyuk0309 <bahngeh@naver.com>
2022-09-19 17:19:43 +0900

    test : 테스트 작성

---
+++ src/test/java/com/meoguri/linkocean/domain/user/service/OAuthAuthenticationServiceTest.java
@@ -0,0 +1,43 @@
+package com.meoguri.linkocean.domain.user.service;
+
+import static com.meoguri.linkocean.domain.user.entity.vo.OAuthType.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.meoguri.linkocean.domain.user.entity.vo.Email;
+import com.meoguri.linkocean.infrastructure.oauth.google.GoogleOAuthClient;
+import com.meoguri.linkocean.test.support.domain.service.BaseServiceTest;
+
+class OAuthAuthenticationServiceTest extends BaseServiceTest {
+
+	@Autowired
+	private OAuthAuthenticationService oAuthAuthenticationService;
+
+	@MockBean
+	private GoogleOAuthClient googleOAuthClient;
+
+	@AfterEach
+	void cleanup() {
+		databaseCleanup.execute();
+	}
+
+	@Test
+	void 사용자_인증_성공() {
+		//given
+		final String authorizationCode = "code";
+
+		given(googleOAuthClient.getUserEmail(any())).willReturn(new Email("email@google.com"));
+
+		//when
+		final String jwt = oAuthAuthenticationService.authenticate(GOOGLE, authorizationCode);
+
+		//then
+		assertThat(jwt).isNotBlank();
+	}
+}
