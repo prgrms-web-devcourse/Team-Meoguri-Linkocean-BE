bb588f5a2c065f23837a560381b06352ee5e824e
hyuk0309 <bahngeh@naver.com>
2022-09-29 13:21:24 +0900

    refactor : Spring의 RedisProperties 사용하기

--- src/main/java/com/meoguri/linkocean/configuration/RedisConfiguration.java
+++ src/main/java/com/meoguri/linkocean/configuration/RedisConfiguration.java
@@ -1,12 +1,11 @@
 package com.meoguri.linkocean.configuration;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-
-import com.meoguri.linkocean.internal.user.infrastructure.redis.RedisProperties;
 
 import lombok.RequiredArgsConstructor;
 
